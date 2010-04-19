scop/bash-completion

commit 8f42adfa8d5e9fbbba9566d1b76d9f89667e6d98

Style policy: don't use fancy globbing in case labels

contrib/xz
@@ -23,7 +23,7 @@ _xz()
     local xspec="*.@(xz|lzma)"
 
     case "$prev" in
-        -@(!(-*)[dlt]*|-decompress|-list|-test))
+        --decompress|--list|--test|!(-*)[dlt]*)
             xspec="!"$xspec
             ;;
         --files|--files0)

commit 7fc1fa88a89b335244f89cc8b74dfca67255faed

Fix handling of short xz options.
Was broken in 8f42adfa8d5e9fbbba9566d1b76d9f89667e6d98

contrib/xz
@@ -23,7 +23,7 @@ _xz()
     local xspec="*.@(xz|lzma|txz|tlz)"
 
     case $prev in
-        --decompress|--list|--test|!(-*)[dlt]*)
+        --decompress|--list|--test|-!(-*)[dlt]*)
             xspec="!"$xspec
             ;;
         --files|--files0)

test/lib/completions/xz.exp
@@ -17,4 +17,17 @@ assert_complete_any "xz "
 sync_after_int
 
 
+assert_complete "a/ bashcomp.lzma bashcomp.tar.xz bashcomp.tlz bashcomp.xz" \
+    "xz -d fixtures/xz/"
+
+
+sync_after_int
+
+
+assert_complete "a/ bashcomp.tar" "xz fixtures/xz/"
+
+
+sync_after_int
+
+
 teardown