e401bec2633837f64c06c914fd724ce796a20633
Ville Skyttä <ville.skytta@iki.fi>
2011-11-01 22:24:13 +0200

    Don't turn off indent-tabs-mode for everything, it's desirable e.g. for Makefiles.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,5 @@
 ;; bash-completion shell-script mode settings for Emacs
 
-((nil     . ((indent-tabs-mode  . nil)))
- (sh-mode . ((sh-basic-offset   . 4)
+((sh-mode . ((indent-tabs-mode  . nil)
+             (sh-basic-offset   . 4)
              (sh-indent-comment . t))))
