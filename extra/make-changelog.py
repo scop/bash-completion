8a9cb54d9769fcfebc37d85d204a5893b7ae0eee
Ville Skyttä <ville.skytta@iki.fi>
2016-03-03 11:50:34 +0200

    make-changelog.py: Make work with Python 3

--- extra/make-changelog.py
+++ extra/make-changelog.py
@@ -5,7 +5,7 @@ import git
 import sys
 from collections import defaultdict
 from textwrap import wrap
-from email.Utils import formatdate
+from email.utils import formatdate
 
 repo = git.Repo('.')
 changelog = defaultdict(list)
@@ -14,13 +14,13 @@ for id in repo.iter_commits('%s..HEAD' % sys.argv[1]):
     commit = repo.commit(id)
     changelog[commit.author.name].append(commit.summary)
 
-print 'bash-completion (X.Y)'
-print
+print('bash-completion (X.Y)')
+print('')
 
 for author in sorted(changelog.keys()):
-    print "  [ %s ]" % author
+    print('  [ %s ]' % author)
     for log in changelog[author]:
-        print '\n'.join(wrap(log, initial_indent='  * ', subsequent_indent='    '))
-    print
+        print('\n'.join(wrap(log, initial_indent='  * ', subsequent_indent='    ')))
+    print('')
 
-print ' -- David Paleino <d.paleino@gmail.com> ', formatdate(localtime=True)
+print(' -- David Paleino <d.paleino@gmail.com> ', formatdate(localtime=True))
