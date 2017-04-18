82d85c1380707461ad8511b95349d8a56b9961ea
Ville Skyttä <ville.skytta@iki.fi>
2017-04-18 11:34:38 +0300

    Travis: Switch tests to docker, update to Ubuntu 14

---
+++ test/docker/docker-script.sh
@@ -0,0 +1,19 @@
+#!/bin/sh -ex
+
+export bashcomp_bash=bash
+env
+
+autoreconf -i
+./configure
+make
+
+make -C completions check
+
+cd test
+./runCompletion --all
+./runInstall --all
+./runUnit --all
+
+cd ..
+mkdir install-test
+make install DESTDIR=$(pwd)/install-test
