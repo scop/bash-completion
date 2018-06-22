03442face6795c56ba0b89eadcc8f7e1ba1b0f14
Ville Skyttä <ville.skytta@iki.fi>
2018-06-22 14:02:10 +0300

    extra/git-post-commit.sh: Add git post-commit Docker Hub trigger hook

---
+++ extra/git-post-commit.sh
@@ -0,0 +1,23 @@
+#!/bin/sh -e
+
+# Post-commit hook for triggering bash-completion Docker Hub test image
+# builds at https://hub.docker.com/r/vskytta/bash-completion/
+#
+# To enable: ln -s ../../extra/git-post-commit.sh .git/hooks/post-commit
+#
+# The bash-completion.docker-hub-trigger-url config option must be set to
+# the full Docker Hub build trigger URL to hit.
+
+url=$(git config bash-completion.docker-hub-trigger-url)
+
+test $(git symbolic-ref --short HEAD) = master
+
+git diff-tree -r --name-only --no-commit-id HEAD | \
+    grep -qxF completions/Makefile.am
+
+curl \
+    --silent --show-error \
+    --max-time 30 \
+    --header Content-Type:application/json \
+    --data '{"build":true}' \
+    $url >/dev/null
