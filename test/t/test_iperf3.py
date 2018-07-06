b3acc9412937abd92a9ff4d7a34515520df56b5c
Ville Skyttä <ville.skytta@iki.fi>
2018-07-06 18:44:24 +0300

    test: Add some iperf, iperf3 and xmodmap test cases

--- test/t/test_iperf3.py
+++ test/t/test_iperf3.py
@@ -3,18 +3,18 @@ import pytest
 
 class Test(object):
 
-    @pytest.mark.complete("iperf ")
+    @pytest.mark.complete("iperf3 ")
     def test_(self, completion):
         assert completion.list
 
-    @pytest.mark.complete("iperf --bind ")
+    @pytest.mark.complete("iperf3 --bind ")
     def test_bind(self, completion):
         assert completion.list
 
-    @pytest.mark.complete("iperf --client foo --")
+    @pytest.mark.complete("iperf3 --client foo --")
     def test_client(self, completion):
         assert completion.list and "--daemon" not in completion.list
 
-    @pytest.mark.complete("iperf --server --")
+    @pytest.mark.complete("iperf3 --server --")
     def test_server(self, completion):
         assert "--daemon" in completion.list
--- test/t/test_xmodmap.py
+++ test/t/test_xmodmap.py
@@ -6,3 +6,7 @@ class Test(object):
     @pytest.mark.complete("xmodmap ")
     def test_(self, completion):
         assert completion.list
+
+    @pytest.mark.complete("xmodmap -")
+    def test_dash(self, completion):
+        assert completion.list
