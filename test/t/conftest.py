4abf2bae1b5fcf58c6b981fcebf907ac83fe7107
Ville Skyttä <ville.skytta@iki.fi>
2018-07-08 21:55:31 +0300

    test: Allow __load_completion to fail
    ...so we can test completions that are directly loaded in
    bash_completion without a separate file.

--- test/t/conftest.py
+++ test/t/conftest.py
@@ -111,7 +111,9 @@ def is_bash_type(bash: pexpect.spawn, cmd: str) -> bool:
 
 def load_completion_for(bash: pexpect.spawn, cmd: str) -> bool:
     try:
-        assert_bash_exec(bash, "__load_completion %s" % cmd)
+        # Allow __load_completion to fail so we can test completions
+        # that are directly loaded in bash_completion without a separate file.
+        assert_bash_exec(bash, "__load_completion %s || :" % cmd)
         assert_bash_exec(bash, "complete -p %s &>/dev/null" % cmd)
     except AssertionError:
         return False
