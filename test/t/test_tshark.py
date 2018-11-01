36095f3c1676694aa7fe84d44c5a29c29630bcc1
Peter Wu <peter@lekensteyn.nl>
2018-11-01 11:12:18 +0100

    tshark: Support preferences (-o) completion with memoization
    Support both completion of `-o tcp....` and `-otcp....`. Memoize the
    tshark output since it can take 1 second with ASAN+Debug. _tshark_prefs
    is 28190 bytes for tshark v2.9.0rc0-2375-gc672124881.

--- test/t/test_tshark.py
+++ test/t/test_tshark.py
@@ -1,6 +1,7 @@
 import pytest
 
 
+@pytest.mark.bashcomp(ignore_env=r"^\+_tshark_prefs=")
 class TestTshark:
 
     @pytest.mark.complete("tshark -")
@@ -14,3 +15,11 @@ class TestTshark:
     @pytest.mark.complete("tshark -O foo,htt")
     def test_3(self, completion):
         assert completion.list
+
+    @pytest.mark.complete("tshark -o tcp")
+    def test_4(self, completion):
+        assert "tcp.desegment_tcp_streams:" in completion.list
+
+    @pytest.mark.complete("tshark -otcp")
+    def test_5(self, completion):
+        assert "-otcp.desegment_tcp_streams:" in completion.list
