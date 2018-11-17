da37fa341903565379523b6e85fb7626997e1879
Ville Skyttä <ville.skytta@iki.fi>
2018-11-17 22:30:04 +0200

    test: Convert portinstall test case to pytest+pexpect (untested)

---
+++ test/t/test_portinstall.py
@@ -0,0 +1,21 @@
+import pytest
+
+from conftest import assert_bash_exec
+
+
+class TestPortinstall:
+
+    @pytest.fixture(scope="class")
+    def portsdir(self, request, bash):
+        assert_bash_exec(bash, "PORTSDIR=$TESTDIR/tmp")
+        assert_bash_exec(bash, "command sed -e s,PORTSDIR,$PORTSDIR,g "
+                         "pkgtools/ports/INDEX.dist >$PORTSDIR/INDEX")
+        assert_bash_exec(bash, "cp $PORTSDIR/INDEX $PORTSDIR/INDEX-5")
+        request.addfinalizer(
+            lambda: assert_bash_exec(bash, "rm $PORTSDIR/INDEX{,-5}"))
+
+    @pytest.mark.complete("portinstall ", env=dict(PORTSDIR="$TESTDIR/tmp"))
+    def test_1(self, completion, portsdir):
+        assert completion.list == \
+            "bash-2.05b.007_6 bash-3.1.17 bash-completion-20060301_2 " \
+            "shells/bash shells/bash-completion shells/bash2".split()
