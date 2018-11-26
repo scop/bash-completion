4f64fb0d7ed7a83cd888e0f25d4050e26c20a060
Ville Skyttä <ville.skytta@iki.fi>
2018-11-26 22:47:06 +0200

    grpck: Parse options with _parse_help, falling back to _parse_usage

--- test/t/test_grpck.py
+++ test/t/test_grpck.py
@@ -6,3 +6,7 @@ class TestGrpck:
     @pytest.mark.complete("grpck ")
     def test_1(self, completion):
         assert completion.list
+
+    @pytest.mark.complete("grpck -")
+    def test_2(self, completion):
+        assert completion.list
