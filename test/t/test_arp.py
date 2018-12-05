a7ece5532328f4901fa366f4ee23d9e3f7488674
Ville Skyttä <ville.skytta@iki.fi>
2018-12-05 23:12:48 +0200

    test: Fix arp CI (non)expectations, remove redundant test case

--- test/t/test_arp.py
+++ test/t/test_arp.py
@@ -5,16 +5,11 @@ import pytest
 
 class TestArp:
 
-    @pytest.mark.complete("arp ")
+    @pytest.mark.xfail(bool(os.environ.get("CI")),
+                       reason="Probably fails in CI")
     def test_1(self, completion):
         assert completion.list
 
     @pytest.mark.complete("arp -")
     def test_2(self, completion):
-        assert completion.list
-
-    @pytest.mark.xfail(bool(os.environ.get("CI")),
-                       reason="Probably fails in CI")
-    @pytest.mark.complete("arp -d ")
-    def test_3(self, completion):
         assert completion.list
