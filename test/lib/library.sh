a4924148f0c846cb8640fbc45e1a466058a16744
Ville Skyttä <ville.skytta@iki.fi>
2018-12-31 13:12:47 +0200

    test: Remove some no longer used old test suite code

--- test/lib/library.sh
+++ test/lib/library.sh
@@ -2,7 +2,6 @@
 
 
 # @param $1  Char to add to $COMP_WORDBREAKS
-# @see remove_comp_wordbreak_char()
 add_comp_wordbreak_char() {
     [[ "${COMP_WORDBREAKS//[^$1]}" ]] || COMP_WORDBREAKS+=$1
 } # add_comp_wordbreak_char()
@@ -37,30 +36,3 @@ echo_array() {
     local name=$1[@]
     printf "%s\n" "${!name}" | sort
 } # echo_array()
-
-
-# Check if current bash version meets specified minimum
-# @param $1  (integer) Major version number
-# @param $2  (integer) Minor version number
-# @param $3  (integer) Patch level
-# @return  0 if success, > 0 if not
-is_bash_version_minimal() {
-    [[      (
-                ${BASH_VERSINFO[0]} -gt $1
-            ) || (
-                ${BASH_VERSINFO[0]} -eq $1 &&
-                ${BASH_VERSINFO[1]} -gt $2
-            ) || (
-                ${BASH_VERSINFO[0]} -eq $1 &&
-                ${BASH_VERSINFO[1]} -eq $2 &&
-                ${BASH_VERSINFO[2]} -ge $3
-            )
-    ]]
-} # is_bash_version_minimal()
-
-
-# @param $1  Char to remove from $COMP_WORDBREAKS
-# @see add_comp_wordbreak_char()
-remove_comp_wordbreak_char() {
-    COMP_WORDBREAKS=${COMP_WORDBREAKS//$1}
-} # remove_comp_wordbreak_char()
