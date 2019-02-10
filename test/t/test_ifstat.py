0b4467343aeaab4d8760943bf26955505814b908
Ville Skyttä <ville.skytta@iki.fi>
2019-02-10 09:49:11 +0200

    ifstat: Make work with iproute2 version

--- test/t/test_ifstat.py
+++ test/t/test_ifstat.py
@@ -7,10 +7,12 @@ class TestIfstat:
     def test_1(self, completion):
         assert completion
 
-    @pytest.mark.complete("ifstat -i ")
+    @pytest.mark.complete("ifstat -i ",
+                          skipif="ifstat -v | command grep -qF iproute2")
     def test_2(self, completion):
         assert completion
 
-    @pytest.mark.complete("ifstat -d ")
+    @pytest.mark.complete("ifstat -d ",
+                          skipif="ifstat -v | command grep -qF iproute2")
     def test_3(self, completion):
         assert completion
