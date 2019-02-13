15fe6395eb447a7e66a7aec07be2963c5f6c2131
Ville Skyttä <ville.skytta@iki.fi>
2019-02-13 20:34:30 +0200

    jsonschema: New completion

---
+++ test/t/test_jsonschema.py
@@ -0,0 +1,12 @@
+import pytest
+
+
+class TestJsonschema:
+
+    @pytest.mark.complete("jsonschema ")
+    def test_1(self, completion):
+        assert completion
+
+    @pytest.mark.complete("jsonschema -")
+    def test_2(self, completion):
+        assert completion
