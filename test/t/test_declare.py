89a0ed758de066dec353687b6506b512f2901cf8
Ville Skyttä <ville.skytta@iki.fi>
2019-02-24 21:01:49 +0200

    test: Fix declare test case with bash 5.0

--- test/t/test_declare.py
+++ test/t/test_declare.py
@@ -13,7 +13,8 @@ class TestDeclare:
 
     @pytest.mark.complete("declare -p BASH_ARG")
     def test_3(self, completion):
-        assert completion == "BASH_ARGC BASH_ARGV".split()
+        # bash 5.0 has BASH_ARGV0 too
+        assert all(x in completion for x in "BASH_ARGC BASH_ARGV".split())
 
     @pytest.mark.complete("declare -f _parse_")
     def test_4(self, completion):
