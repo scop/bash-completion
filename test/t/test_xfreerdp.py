5f4f529ed3755a682e999d93f87dc5ac518ab51f
Ville Skyttä <ville.skytta@iki.fi>
2019-03-08 22:16:46 +0200

    xfreerdp: reinstate support for old versions with dash option syntax

--- test/t/test_xfreerdp.py
+++ test/t/test_xfreerdp.py
@@ -1,10 +1,25 @@
 import pytest
 
+from conftest import assert_bash_exec
+
 
 class TestXfreerdp:
 
+    def _help(self, bash):
+        return assert_bash_exec(bash, "xfreerdp --help || :", want_output=True)
+
+    @pytest.fixture(scope="class")
+    def slash_syntax(self, bash):
+        if "/help" not in self._help(bash):
+            pytest.skip("Not slash syntax")
+
+    @pytest.fixture(scope="class")
+    def dash_syntax(self, bash):
+        if "/help" in self._help(bash):
+            pytest.skip("Not dash syntax")
+
     @pytest.mark.complete("xfreerdp /")
-    def test_1(self, completion):
+    def test_1(self, bash, completion, slash_syntax):
         assert completion
 
     @pytest.mark.complete("xfreerdp -")
@@ -12,13 +27,21 @@ class TestXfreerdp:
         assert completion
 
     @pytest.mark.complete("xfreerdp +")
-    def test_3(self, completion):
+    def test_3(self, bash, completion, slash_syntax):
         assert completion
 
     @pytest.mark.complete("xfreerdp /kbd:")
-    def test_4(self, completion):
+    def test_4(self, bash, completion, slash_syntax):
         assert completion
 
     @pytest.mark.complete("xfreerdp /help ")
     def test_5(self, completion):
+        assert not completion
+
+    @pytest.mark.complete("xfreerdp -k ")
+    def test_6(self, bash, completion, dash_syntax):
+        assert completion
+
+    @pytest.mark.complete("xfreerdp --help ")
+    def test_7(self, completion):
         assert not completion
