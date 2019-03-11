dd80f35279afd4f056dc191767b9869c9649d476
Ville Skyttä <ville.skytta@iki.fi>
2019-03-11 21:26:24 +0200

    _longopt: don't complete --no-* with file/dirname arg
    Closes https://github.com/scop/bash-completion/pull/291
    
    Thanks-to: Gabriel F. T. Gomes <gabriel@inconstante.eti.br>

--- test/t/test_grep.py
+++ test/t/test_grep.py
@@ -6,3 +6,12 @@ class TestGrep:
     @pytest.mark.complete("grep --")
     def test_1(self, completion):
         assert completion
+
+    @pytest.mark.complete("grep --no-complete-dir f", cwd="shared/default")
+    def test_2(self, completion):
+        """
+        Test --no-*dir isn't restricted to dirs only.
+
+        Not really a grep option, but tests _longopt.
+        """
+        assert completion == "foo foo.d/".split()
