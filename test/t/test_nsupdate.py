80ba367c4e62c63927b2ffe6884ed9756ace5195
Ville Skyttä <ville.skytta@iki.fi>
2019-03-20 06:53:59 +0200

    nsupdate: new completion

---
+++ test/t/test_nsupdate.py
@@ -0,0 +1,12 @@
+import pytest
+
+
+class TestNsupdate:
+
+    @pytest.mark.complete("nsupdate ")
+    def test_1(self, completion):
+        assert completion
+
+    @pytest.mark.complete("nsupdate -")
+    def test_2(self, completion):
+        assert completion
