fd37d6f506013a42d35841b88d19851f192cf4ad
Ville Skyttä <ville.skytta@iki.fi>
2019-04-14 11:46:35 +0300

    shellcheck: new completion

---
+++ test/t/test_shellcheck.py
@@ -0,0 +1,20 @@
+import pytest
+
+
+class TestShellcheck:
+
+    @pytest.mark.complete("shellcheck ")
+    def test_1(self, completion):
+        assert completion
+
+    @pytest.mark.complete("shellcheck -")
+    def test_2(self, completion):
+        assert completion
+
+    @pytest.mark.complete("shellcheck --format=")
+    def test_3(self, completion):
+        assert completion
+
+    @pytest.mark.complete("shellcheck -s ")
+    def test_4(self, completion):
+        assert "bash" in completion
