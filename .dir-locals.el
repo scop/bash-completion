77f2855e10f08e9789908d7a91d42c407b67ea32
Ville Skyttä <ville.skytta@iki.fi>
2019-04-20 09:44:26 +0300

    .dir-locals.el: use flycheck-sh-bash-args
    Requires flycheck > 31, but this approach doesn't break checking when
    shellcheck is installed.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,10 +1,6 @@
-;; bash-completion shell-script mode additional settings for Emacs
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
 
-((sh-mode . ((sh-indent-comment . t)
-             ;; Somewhat fragile, waiting for flycheck-sh-bash-args
-             (eval . (setq flycheck-command-wrapper-function
-                           (lambda (command)
-                             (append (butlast command 1)
-                                     '("-O" "extglob")
-                                     (last command)))))
-             )))
+((sh-mode
+  (flycheck-sh-bash-args "-O" "extglob")
+  (sh-indent-comment . t)))
