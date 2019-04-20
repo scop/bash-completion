d2bec62f609e4ce4117ba74d5a81a45ad8e23369
Ville Skyttä <ville.skytta@iki.fi>
2019-04-20 22:36:34 +0300

    test: port some more ssh test case to pytest+pexpect

--- test/t/test_ssh.py
+++ test/t/test_ssh.py
@@ -5,3 +5,17 @@ class TestSsh:
     @pytest.mark.complete("ssh -Fsp", cwd="ssh")
     def test_1(self, completion):
         assert completion == "-Fspaced  conf"
+
+    @pytest.mark.complete("ssh -F config ls", cwd="ssh")
+    def test_2(self, completion):
+        """Should complete both commands and hostname."""
+        assert all(x in completion for x in "ls ls_known_host".split())
+
+    @pytest.mark.complete("ssh bash", cwd="ssh")
+    def test_3(self, completion):
+        """
+        First arg should not complete with commands.
+
+        Assumes there's no "bash" known host.
+        """
+        assert "bash" not in completion
