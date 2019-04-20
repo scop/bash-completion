c59a2d0950cb5b20da46598889f31bc17ac3658c
Ville Skyttä <ville.skytta@iki.fi>
2019-04-20 16:14:03 +0300

    gdb: relax core filename pattern
    Closes #300

--- test/t/test_gdb.py
+++ test/t/test_gdb.py
@@ -5,3 +5,11 @@ class TestGdb:
     @pytest.mark.complete("gdb - ")
     def test_1(self, completion):
         assert completion
+
+    @pytest.mark.complete("gdb foo ", cwd="gdb")
+    def test_2(self, completion):
+        assert completion == sorted(
+            "core core.12345 "
+            "core.weston.1000.bc46148827504908898ad152f43bffb2.5308."
+            "1555362132000000".split()
+        )
