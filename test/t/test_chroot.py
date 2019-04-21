82ffbb2d157bc401c0838fe98146fa89ecf8c56b
Ville Skyttä <ville.skytta@iki.fi>
2019-04-21 10:57:46 +0300

    various: apply file vs dir special cases also when invoked with full path
    Affects autoheader, autoupdate, chroot, ci, rmdir, and mkdir.

--- test/t/test_chroot.py
+++ test/t/test_chroot.py
@@ -5,3 +5,8 @@ class TestChroot:
     @pytest.mark.complete("chroot ")
     def test_1(self, completion):
         assert completion
+
+    @pytest.mark.complete("/bin/chroot shared/default/")
+    def test_2(self, completion):
+        """Should complete dirs only, also when invoked using full path."""
+        assert completion == ["bar bar.d/", "foo.d/"]
--- test/t/test_rmdir.py
+++ test/t/test_rmdir.py
@@ -6,6 +6,7 @@ class TestRmdir:
     def test_1(self, completion):
         assert completion
 
-    @pytest.mark.complete("rmdir shared/default/")
+    @pytest.mark.complete("/bin/rmdir shared/default/")
     def test_2(self, completion):
+        """Should complete dirs only, also when invoked using full path."""
         assert completion == ["bar bar.d/", "foo.d/"]
