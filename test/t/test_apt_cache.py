6e2ddea986a3462070e6eafb098fc3d3f16bd835
Ville Skyttä <ville.skytta@iki.fi>
2019-04-22 13:19:35 +0300

    apt-cache: protect showsrc against regex specials

--- test/t/test_apt_cache.py
+++ test/t/test_apt_cache.py
@@ -6,3 +6,8 @@ class TestAptCache:
     @pytest.mark.complete("apt-cache ")
     def test_1(self, completion):
         assert completion
+
+    @pytest.mark.complete("apt-cache showsrc [")
+    def test_2(self, completion):
+        # Doesn't actually fail on grep errors, but takes a long time.
+        assert not completion
