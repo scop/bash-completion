e0bb03a588ba57a77424a19f690c1413b3de8086
Ville Skyttä <ville.skytta@iki.fi>
2019-04-23 22:00:33 +0300

    test: add invoke-rc.d test case for not repeating already given options

--- test/t/test_invoke_rc_d.py
+++ test/t/test_invoke_rc_d.py
@@ -6,3 +6,9 @@ class TestInvokeRcD:
     @pytest.mark.complete("invoke-rc.d ")
     def test_1(self, completion):
         assert completion
+
+    @pytest.mark.complete("invoke-rc.d --no-fallback --")
+    def test_2(self, completion):
+        """Test already specified option is not offered."""
+        assert completion
+        assert "--no-fallback" not in completion
