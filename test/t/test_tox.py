468436d5a64f8a85b3d71a3c0a64042b7ca21307
Jay Kamat <jaygkamat@gmail.com>
2019-04-23 21:27:26 -0700

    Add filename completion as fallback for tox
    Many tox commands sometimes take filenames as arguments to the command
    itself. For example:
    
    tox -e env -- -foo -bar -baz tests/foo/bar/baz

--- test/t/test_tox.py
+++ test/t/test_tox.py
@@ -13,3 +13,19 @@ class TestTox:
     @pytest.mark.complete("tox -e foo,")
     def test_3(self, completion):
         assert completion == "foo,ALL"
+
+    @pytest.mark.complete("tox -e foo -- ")
+    def test_4(self, completion):
+        """Test that ending in '-- ' triggers filename completion."""
+        assert completion
+
+    @pytest.mark.complete("tox -e foo -- -foo -bar ")
+    def test_5(self, completion):
+        """Test that arguments after '--' trigger filename completion."""
+        assert completion
+
+    @pytest.mark.complete("tox -e foo --")
+    def test_6(self, completion):
+        """Test that ending in '--' does not trigger filename completion."""
+        for c in completion:
+            assert c.startswith("--")
