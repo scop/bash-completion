40be1f491bfbeec50787cbe6bd2c6a794b19ef46
Ville Skyttä <ville.skytta@iki.fi>
2019-05-01 13:50:17 +0300

    iconv: weed out ... from encoding completions
    The Alpine Linux one outputs that at end of its list of supported
    encodings,
    https://git.alpinelinux.org/aports/tree/main/musl/iconv.c?id=dbf9f3832723d92fe77a807f177d8826d07c9145

--- test/t/test_iconv.py
+++ test/t/test_iconv.py
@@ -13,3 +13,7 @@ class TestIconv:
     @pytest.mark.complete("iconv ")
     def test_3(self, completion):
         assert completion
+
+    @pytest.mark.complete("iconv -f ")
+    def test_4(self, completion):
+        assert "..." not in completion
