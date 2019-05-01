0ba7af221b3ec3ec7b1efecd3c8458068f1934b3
Ville Skyttä <ville.skytta@iki.fi>
2019-05-01 13:47:06 +0300

    test_iconv: add basic file completion test

--- test/t/test_iconv.py
+++ test/t/test_iconv.py
@@ -9,3 +9,7 @@ class TestIconv:
     @pytest.mark.complete("iconv -f UTF")
     def test_2(self, completion):
         assert completion
+
+    @pytest.mark.complete("iconv ")
+    def test_3(self, completion):
+        assert completion
