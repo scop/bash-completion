2deda5b49d1fe722008c3d676a4e95a551237586
Ville Skyttä <ville.skytta@iki.fi>
2019-05-09 01:47:12 +0700

    postfix: option completion is expected to fail at the moment

--- test/t/test_postfix.py
+++ test/t/test_postfix.py
@@ -6,6 +6,7 @@ class TestPostfix:
     def test_1(self, completion):
         assert completion
 
+    @pytest.mark.xfail  # see TODO in completion
     @pytest.mark.complete("postfix -")
     def test_2(self, completion):
         assert completion
