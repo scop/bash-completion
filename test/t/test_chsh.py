696f90d30a5cd6769be0affc166b2003de8a44e7
Ville Skyttä <ville.skytta@iki.fi>
2019-05-09 00:46:38 +0700

    chsh, pwck: try _parse_help before _parse_usage

--- test/t/test_chsh.py
+++ test/t/test_chsh.py
@@ -9,3 +9,7 @@ class TestChsh:
     @pytest.mark.complete("chsh -s ")
     def test_2(self, completion):
         assert completion
+
+    @pytest.mark.complete("chsh -")
+    def test_3(self, completion):
+        assert completion
