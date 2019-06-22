5fd2701371a491176b25fa9683481d6d143675f2
Ville Skyttä <ville.skytta@iki.fi>
2019-06-22 11:23:34 +0200

    test: expect failures for bc without --help useful with _longopt

--- test/t/test_bc.py
+++ test/t/test_bc.py
@@ -2,6 +2,12 @@ import pytest
 
 
 class TestBc:
-    @pytest.mark.complete("bc --")
+    @pytest.mark.complete(
+        "bc --",
+        xfail=(
+            "! bc --help &>/dev/null || "
+            "! bc --help 2>&1 | command grep -qF -- --help"
+        ),
+    )
     def test_1(self, completion):
         assert completion
