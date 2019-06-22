07eada37598e19c282081102ad29d62a6e87e9f4
Ville Skyttä <ville.skytta@iki.fi>
2019-06-22 14:32:04 +0200

    test: avoid gnome-mplayer core dump on Ubuntu 14
    XDG_DATA_DIRS set to a dir with no schemas results in "GLib-GIO-ERROR
    **: No GSettings schemas are installed on the system" and a core dump.
    We currently have it set to /var/empty to avoid files installed on the
    system being loaded and possibly affecting tests.

--- test/t/test_gnome_mplayer.py
+++ test/t/test_gnome_mplayer.py
@@ -1,12 +1,15 @@
 import pytest
 
 
-@pytest.mark.bashcomp(cmd="gnome-mplayer")
+@pytest.mark.bashcomp(cmd="gnome-mplayer", ignore_env=r"^[+-]XDG_DATA_DIRS=")
 class TestGnomeMplayer:
     @pytest.mark.complete("gnome-mplayer ")
     def test_1(self, completion):
         assert completion
 
-    @pytest.mark.complete("gnome-mplayer -")
+    # XDG_DATA_DIRS set to a dir with no schemas results in
+    # "GLib-GIO-ERROR **: No GSettings schemas are installed on the system"
+    # and a core dump on --help on Ubuntu 14.
+    @pytest.mark.complete("gnome-mplayer -", pre_cmds=("unset XDG_DATA_DIRS",))
     def test_2(self, completion):
         assert completion
