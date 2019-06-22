5c0e98832a1ee4e2829d47e2fb22f495ac09b641
Ville Skyttä <ville.skytta@iki.fi>
2019-06-22 14:14:56 +0200

    xvfb-run: new completion

---
+++ test/t/test_xvfb_run.py
@@ -0,0 +1,12 @@
+import pytest
+
+
+@pytest.mark.bashcomp(cmd="xvfb-run")
+class TestXvfbRun:
+    @pytest.mark.complete("xvfb-run ")
+    def test_no_args(self, completion):
+        assert any(x in completion for x in ("bash", "xvfb-run"))
+
+    @pytest.mark.complete("xvfb-run -")
+    def test_options(self, completion):
+        assert completion
