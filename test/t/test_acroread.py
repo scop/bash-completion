1f7fdc67d3e015156d4622e6904a728f3717dadb
Ville Skyttä <ville.skytta@iki.fi>
2019-06-24 07:45:47 -0400

    test: fix acroread fixture dir

--- test/t/test_acroread.py
+++ test/t/test_acroread.py
@@ -2,6 +2,6 @@ import pytest
 
 
 class TestAcroread:
-    @pytest.mark.complete("acroread ", cwd="fixtures/acroread")
+    @pytest.mark.complete("acroread ", cwd="acroread")
     def test_1(self, completion):
         assert completion == "foo.d/ t.pdf".split()
