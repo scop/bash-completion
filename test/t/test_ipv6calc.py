151ac18040eb27533ebca4815c6286e2d758c0f5
Ville Skyttä <ville.skytta@iki.fi>
2019-06-29 10:07:20 -0400

    ipv6calc: parse help instead of hardcoding option list

--- test/t/test_ipv6calc.py
+++ test/t/test_ipv6calc.py
@@ -4,7 +4,7 @@ import pytest
 class TestIpv6calc:
     @pytest.mark.complete("ipv6calc -")
     def test_1(self, completion):
-        assert completion
+        assert "--action" in completion
 
     @pytest.mark.complete("ipv6calc --in ")
     def test_2(self, completion):
