c3ae3f425e6a48427b7fde9a0099e90a19a77f12
Ville Skyttä <ville.skytta@iki.fi>
2019-06-30 16:57:25 -0400

    Install nginx for service tests

--- install-packages.sh
+++ install-packages.sh
@@ -4,6 +4,10 @@ cd ${TMPDIR:-/tmp}
 
 apk upgrade
 
-# Nothing else here, at least yet. Useful this way already in order to
-# test some very base executables, as well as ones tha come from
+# Nothing much here, at least yet. Useful this way already in order to
+# test some very base executables, as well as ones that come from
 # busybox. Don't lose that if adding stuff here!
+
+# An arbitrary package containing an init script or the like for
+# testing service completion.
+apk add nginx
