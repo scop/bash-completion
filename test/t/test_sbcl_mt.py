9838cbfaf9e508b1ec1ad05959d89f3cf9e5d719
Ville Skyttä <ville.skytta@iki.fi>
2019-06-30 16:50:56 -0400

    test: mark sbcl-mt xfail due to whitespace split issues

--- test/t/test_sbcl_mt.py
+++ test/t/test_sbcl_mt.py
@@ -3,6 +3,7 @@ import pytest
 
 @pytest.mark.bashcomp(cmd="sbcl-mt")
 class TestSbclMt:
+    @pytest.mark.xfail  # TODO: whitespace split issue
     @pytest.mark.complete("sbcl-mt shared/default/")
     def test_1(self, completion):
         assert completion == ["bar", "bar bar.d/", "foo", "foo foo.d/"]
