bebe43d6e7881d39e9c03faadf2927fa08042241
Ville Skyttä <ville.skytta@iki.fi>
2019-07-13 11:00:28 +0300

    gprof: _parse_usage, drop hardcoded option list

--- test/t/test_gprof.py
+++ test/t/test_gprof.py
@@ -2,6 +2,6 @@ import pytest
 
 
 class TestGprof:
-    @pytest.mark.complete("gprof --", xfail="! gprof --help &>/dev/null")
+    @pytest.mark.complete("gprof --", require_cmd=True)
     def test_1(self, completion):
         assert completion
