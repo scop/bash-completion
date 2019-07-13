29157d07aafac3938fb26e93c542288d027eca9a
Ville Skyttä <ville.skytta@iki.fi>
2019-07-13 11:11:33 +0300

    lintian-info: _parse_help, add more option arg (non)completions

--- test/t/test_lintian_info.py
+++ test/t/test_lintian_info.py
@@ -7,6 +7,6 @@ class TestLintianInfo:
     def test_1(self, completion):
         assert completion
 
-    @pytest.mark.complete("lintian-info --")
+    @pytest.mark.complete("lintian-info --", require_cmd=True)
     def test_2(self, completion):
         assert completion
