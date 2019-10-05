272828a649ca985713f2e9c8388d2cf513bad885
Grisha Levit <grishalevit@gmail.com>
2019-10-04 22:55:36 -0400

    bind: add variable and fix function completion
    Add completion to the common invocation forms of:
        bind 'set variable-name value'
    and
        bind '"keyseq":function-name'

--- test/t/test_bind.py
+++ test/t/test_bind.py
@@ -6,6 +6,10 @@ class TestBind:
     def test_1(self, completion):
         assert completion
 
-    @pytest.mark.complete("bind k")
+    @pytest.mark.complete("bind -q kill-w")
     def test_2(self, completion):
-        assert completion
+        assert all(x in completion for x in "kill-whole-line kill-word".split())
+
+    @pytest.mark.complete("bind 'x:ab")
+    def test_3(self, completion):
+        assert "x:abort" in completion
