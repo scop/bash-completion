9a891c8ebfdd6061f6c1fdabf9711eca148becba
Grisha Levit <grishalevit@gmail.com>
2019-10-06 22:06:38 -0400

    black reformat

--- test/t/test_bind.py
+++ test/t/test_bind.py
@@ -8,8 +8,9 @@ class TestBind:
 
     @pytest.mark.complete("bind -q kill-w")
     def test_2(self, completion):
-        assert all(x in completion for x in
-                   "kill-whole-line kill-word".split())
+        assert all(
+            x in completion for x in "kill-whole-line kill-word".split()
+        )
 
     @pytest.mark.complete("bind 'x:ab")
     def test_3(self, completion):
