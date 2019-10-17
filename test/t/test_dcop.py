73e8faf4d987aef26d52c2354d4336596a7cee92
Ville Skyttä <ville.skytta@iki.fi>
2019-10-17 22:57:35 +0300

    test: mark dcop and mr testcases requiring the cmd as such

--- test/t/test_dcop.py
+++ test/t/test_dcop.py
@@ -1,14 +1,7 @@
-import subprocess
-
 import pytest
 
 
 class TestDcop:
-    @pytest.mark.complete("dcop ")
+    @pytest.mark.complete("dcop ", require_cmd=True)
     def test_1(self, completion):
-        try:
-            subprocess.check_call("dcop &>/dev/null", shell=True)
-        except BaseException:
-            assert not completion
-        else:
-            assert completion
+        assert completion
--- test/t/test_mr.py
+++ test/t/test_mr.py
@@ -9,7 +9,9 @@ class TestMr:
     # man -h tests below: Some mr versions require man to be around in order
     # to provide useful output.
 
-    @pytest.mark.complete("mr --", xfail="! man -h &>/dev/null")
+    @pytest.mark.complete(
+        "mr --", require_cmd=True, xfail="! man -h &>/dev/null"
+    )
     def test_2(self, completion):
         assert completion
 
@@ -20,24 +22,34 @@ class TestMr:
         assert completion == "shared/default/foo.d/foo"
 
     @pytest.mark.complete(
-        "mr bootstrap shared/default/", xfail="! man -h &>/dev/null"
+        "mr bootstrap shared/default/",
+        require_cmd=True,
+        xfail="! man -h &>/dev/null",
     )
     def test_4(self, completion):
         assert completion == ["bar", "bar bar.d/", "foo", "foo.d/"]
 
     @pytest.mark.xfail  # "clean" doesn't exist before mr 1.20141023
-    @pytest.mark.complete("mr clean -", xfail="! man -h &>/dev/null")
+    @pytest.mark.complete(
+        "mr clean -", require_cmd=True, xfail="! man -h &>/dev/null"
+    )
     def test_5(self, completion):
         assert completion == "-f"
 
-    @pytest.mark.complete("mr commit -", xfail="! man -h &>/dev/null")
+    @pytest.mark.complete(
+        "mr commit -", require_cmd=True, xfail="! man -h &>/dev/null"
+    )
     def test_6(self, completion):
         assert completion == "-m"
 
-    @pytest.mark.complete("mr status ", xfail="! man -h &>/dev/null")
+    @pytest.mark.complete(
+        "mr status ", require_cmd=True, xfail="! man -h &>/dev/null"
+    )
     def test_7(self, completion):
         assert not completion
 
-    @pytest.mark.complete("mr run ", xfail="! man -h &>/dev/null")
+    @pytest.mark.complete(
+        "mr run ", require_cmd=True, xfail="! man -h &>/dev/null"
+    )
     def test_8(self, completion):
         assert completion
