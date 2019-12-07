5b12f1d49bf036f99ccd97df2c3effee63fb04d8
Ville Skyttä <ville.skytta@iki.fi>
2019-12-07 09:08:59 +0200

    lilo: honor -C when completing labels

--- test/t/test_lilo.py
+++ test/t/test_lilo.py
@@ -5,3 +5,13 @@ class TestLilo:
     @pytest.mark.complete("lilo -")
     def test_1(self, completion):
         assert completion
+
+    @pytest.mark.complete("lilo -C lilo/lilo.conf -D ")
+    def test_labels(self, completion):
+        assert completion == sorted(
+            "try 2.4.33 tamu PCDOS WinXP oldDOS".split()
+        )
+
+    @pytest.mark.complete("lilo -C -D ")
+    def test_labels_incorrect_command(self, completion):
+        assert not completion
