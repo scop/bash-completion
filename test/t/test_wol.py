8130f87b4e035e3b439ce2f06bdb6bb495288e15
Ville Skyttä <ville.skytta@iki.fi>
2019-12-08 10:06:11 +0200

    test/wol: don't fail MAC test if test system has /etc/ethers entries
    Closes https://github.com/scop/bash-completion/issues/371

--- test/t/test_wol.py
+++ test/t/test_wol.py
@@ -5,14 +5,15 @@ import pytest
 class TestWol:
     @pytest.mark.complete("wol ")
     def test_1(self, completion):
-        assert (
-            completion == "00:00:00:00:00:00 11:11:11:11:11:11 "
+        assert all(
+            x in completion
+            for x in "00:00:00:00:00:00 11:11:11:11:11:11 "
             "22:22:22:22:22:22 33:33:33:33:33:33".split()
         )
 
     @pytest.mark.complete("wol 00:")
     def test_2(self, completion):
-        assert completion == "00:00:00:00:00:00"
+        assert any(x.endswith("00:00:00:00:00") for x in completion)
 
     @pytest.mark.complete("wol -", require_cmd=True)
     def test_3(self, completion):
