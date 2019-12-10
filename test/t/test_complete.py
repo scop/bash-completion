1e029e81dc0a121f575d35c925cc878448feccc4
Ville Skyttä <ville.skytta@iki.fi>
2019-12-10 21:46:23 +0200

    *: complete commands when prefixed with a backslash
    Closes https://github.com/scop/bash-completion/issues/368

--- test/t/test_complete.py
+++ test/t/test_complete.py
@@ -5,3 +5,7 @@ class TestComplete:
     @pytest.mark.complete("complete -")
     def test_1(self, completion):
         assert completion
+
+    @pytest.mark.complete(r"\complete -")
+    def test_2(self, completion):
+        assert completion
