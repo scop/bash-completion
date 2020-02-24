9982cc34d6ea684720f171dcdfa1a20a0b5a81e3
Felix Yan <felixonmars@archlinux.org>
2020-02-25 06:00:13 +0800

    ip: improve completion of route subcommands (#326)
    Implemented frequently used completions for via, dev, and existing routes.
    
    Co-authored-by: Ville Skyttä <ville.skytta@iki.fi>

--- test/t/test_ip.py
+++ test/t/test_ip.py
@@ -9,3 +9,7 @@ class TestIp:
     @pytest.mark.complete("ip a ")
     def test_2(self, completion):
         assert completion
+
+    @pytest.mark.complete("ip route replace ")
+    def test_r_r(self, completion):
+        assert completion
