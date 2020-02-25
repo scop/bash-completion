014b3f2a0e13e87f25f31fd0ccacbf09ba3bcec2
Ville Skyttä <ville.skytta@iki.fi>
2020-02-25 08:02:52 +0200

    printenv: new completion

---
+++ test/t/test_printenv.py
@@ -0,0 +1,15 @@
+import pytest
+
+
+class TestPrintenv:
+    @pytest.mark.complete("printenv ")
+    def test_empty(self, completion):
+        assert completion
+
+    @pytest.mark.complete("printenv PAT")
+    def test_path(self, completion):
+        assert "PATH" in completion
+
+    @pytest.mark.complete("printenv -")
+    def test_options(self, completion):
+        assert completion
