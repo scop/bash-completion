4c64a1bd293b6fe1b5ed182ac8306ed2036b2603
Ville Skyttä <ville.skytta@iki.fi>
2020-03-01 15:50:29 +0200

    test: require openssl command for option argument tests

--- test/t/test_openssl.py
+++ test/t/test_openssl.py
@@ -6,11 +6,11 @@ class TestOpenssl:
     def test_1(self, completion):
         assert completion
 
-    @pytest.mark.complete("openssl pkey -cipher ")
+    @pytest.mark.complete("openssl pkey -cipher ", require_cmd=True)
     def test_2(self, completion):
         assert completion
 
-    @pytest.mark.complete("openssl dgst -s")
+    @pytest.mark.complete("openssl dgst -s", require_cmd=True)
     def test_3(self, completion):
         assert completion
         assert any(x.startswith("-sha") for x in completion)
