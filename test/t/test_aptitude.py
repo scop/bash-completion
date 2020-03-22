762ee12d24db419f02fc026e063962dde82e7275
Ville Skyttä <ville.skytta@iki.fi>
2020-03-22 09:42:32 +0200

    test/aptitude: add some test cases

--- test/t/test_aptitude.py
+++ test/t/test_aptitude.py
@@ -5,3 +5,19 @@ class TestAptitude:
     @pytest.mark.complete("aptitude ")
     def test_1(self, completion):
         assert completion
+
+    @pytest.mark.complete("aptitude -")
+    def test_options(self, completion):
+        assert completion
+
+    @pytest.mark.complete("aptitude --")
+    def test_long_options(self, completion):
+        assert completion
+
+    @pytest.mark.complete("aptitude -u -")
+    def test_no_i_with_u(self, completion):
+        assert "-i" not in completion
+
+    @pytest.mark.complete("aptitude -i -")
+    def test_no_u_with_i(self, completion):
+        assert "-u" not in completion
