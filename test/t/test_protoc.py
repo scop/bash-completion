a10adf25a75bab2d9cd1eb5805e73d12149cb287
Ville Skyttä <ville.skytta@iki.fi>
2020-03-29 09:24:11 +0300

    protoc: complete all --*_out without more specific handling with dirs

--- test/t/test_protoc.py
+++ test/t/test_protoc.py
@@ -9,3 +9,12 @@ class TestProtoc:
     @pytest.mark.complete("protoc -", require_cmd=True)
     def test_2(self, completion):
         assert completion
+        assert any(
+            x.endswith("_out") or x.endswith("_out=") for x in completion
+        )
+
+    @pytest.mark.complete(
+        "protoc --non_existent_plugin_out ", cwd="shared/default"
+    )
+    def test_all_out(self, completion):
+        assert completion == ["bar bar.d/", "foo.d/"]
