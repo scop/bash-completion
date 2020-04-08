3f340bf8d7f63f86987a3c947db272ccd296487e
Ville Skyttä <ville.skytta@iki.fi>
2020-04-08 21:47:04 +0300

    quote_readline: fix $ret leak

---
+++ test/t/unit/test_unit_quote_readline.py
@@ -0,0 +1,15 @@
+import pytest
+
+from conftest import assert_bash_exec
+
+
+@pytest.mark.bashcomp(cmd=None)
+class TestUnitQuoteReadline:
+    def test_exec(self, bash):
+        assert_bash_exec(bash, "quote_readline >/dev/null")
+
+    def test_env_non_pollution(self, bash):
+        """Test environment non-pollution, detected at teardown."""
+        assert_bash_exec(
+            bash, "foo() { quote_readline meh >/dev/null; }; foo; unset foo"
+        )
