e649ea16374bbacfdb84c3d9803211754f62bfc1
Ville Skyttä <ville.skytta@iki.fi>
2020-04-17 09:26:48 +0300

    pgrep, pkill: add --ns and --nslist arg completions

--- test/t/test_pgrep.py
+++ test/t/test_pgrep.py
@@ -11,3 +11,25 @@ class TestPgrep:
     @pytest.mark.complete("pgrep -", require_cmd=True)
     def test_2(self, completion):
         assert completion
+
+    @pytest.mark.complete(
+        "pgrep --nslist ",
+        require_cmd=True,
+        skipif=(
+            "! pgrep --help 2>&1 | command grep -qF 'Available namespaces'"
+        ),
+    )
+    def test_nslist(self, completion):
+        assert completion
+        assert not any("," in x for x in completion)
+
+    @pytest.mark.complete(
+        "pgrep --nslist foo,",
+        require_cmd=True,
+        skipif=(
+            "! pgrep --help 2>&1 | command grep -qF 'Available namespaces'"
+        ),
+    )
+    def test_nslist_after_comma(self, completion):
+        assert completion
+        assert not any("," in x for x in completion)
