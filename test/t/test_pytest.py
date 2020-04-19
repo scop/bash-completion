f4ac16016d005d91652226272e3d496ede8e5ca1
Ville Skyttä <ville.skytta@iki.fi>
2020-04-19 11:33:29 +0300

    pytest: complete test classes

--- test/t/test_pytest.py
+++ test/t/test_pytest.py
@@ -9,3 +9,8 @@ class TestPytest:
     @pytest.mark.complete("pytest -")
     def test_2(self, completion):
         assert completion
+
+    @pytest.mark.complete("pytest ../t/test_pytest.py:")
+    def test_classes(self, completion):
+        assert len(completion) == 1
+        assert next(iter(completion)).endswith("::TestPytest")
