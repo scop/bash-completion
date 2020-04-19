25b4f1db77f2373f7a700998327e9d9d837da89a
Ville Skyttä <ville.skytta@iki.fi>
2020-04-19 11:46:09 +0300

    test/cd: fix test_dir_at_point for setups that repeat "trailer"

--- test/t/test_cd.py
+++ test/t/test_cd.py
@@ -26,12 +26,24 @@ class TestCd:
         assert not completion  # No subdirs nor CDPATH
 
     def test_dir_at_point(self, bash):
-        cmd = "cd shared/default/foo\002\002\002"  # \002 = ^B = cursor left
+        trail = "foo"
+        cmd = "cd shared/default/%s%s" % (
+            trail,
+            "\002" * len(trail),  # \002 = ^B = cursor left
+        )
         bash.send(cmd + "\t")
         bash.expect_exact(cmd.replace("\002", "\b"))
         bash.send(MAGIC_MARK)
-        bash.expect(
-            r"\r\nbar bar\.d/\s+foo\.d/\r\n"
-            + re.escape(PS1 + cmd.replace("\002", "\b") + MAGIC_MARK)
-            + "foo\b\b\b"
-        )
+        bash.expect(r"\r\nbar bar\.d/\s+foo\.d/\r\n")
+        bash.expect_exact(PS1 + cmd.replace("\002", "\b"))
+
+        # At this point, something weird happens. For most test setups, as
+        # expected (pun intended!), MAGIC_MARK follows as is. But for some
+        # others (e.g. CentOS 6, Ubuntu 14 test containers), we get MAGIC_MARK
+        # one character a time, followed each time by foo\b\b\b. Don't know
+        # why, but accept it until/if someone finds out. Or just be fine with
+        # it indefinitely, the visible and practical end result on a terminal
+        # is the same anyway.
+        repeat = "(%s%s)?" % (re.escape(trail), "\b" * len(trail))
+        expected = "".join("%s%s" % (re.escape(x), repeat) for x in MAGIC_MARK)
+        bash.expect(expected)
