f81d313138cb6ab0fd155089b3c7499d733c118b
Ville Skyttä <ville.skytta@iki.fi>
2020-04-19 12:07:16 +0300

    test/alias: port remaining test case to pytest+pexpect

--- test/t/test_alias.py
+++ test/t/test_alias.py
@@ -1,5 +1,7 @@
 import pytest
 
+from conftest import complete_at_point
+
 
 @pytest.mark.bashcomp(
     pre_cmds=("unalias -a", "alias foo=bar", "alias bar='foo foo'"),
@@ -15,3 +17,8 @@ class TestAlias:
     def test_2(self, completion):
         assert completion == "foo='bar'"
         assert not completion.endswith(" ")
+
+    def test_alias_at_point(self, bash):
+        assert complete_at_point(
+            bash=bash, cmd="alias ", trail="foo", expected=r"bar\s+foo\s*?"
+        )
