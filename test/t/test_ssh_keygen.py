6666ebbb0d9934fd999937c10028f51295be5d28
Ville Skyttä <ville.skytta@iki.fi>
2020-04-24 18:05:10 +0300

    ssh-keygen: -s and -n completion improvements

--- test/t/test_ssh_keygen.py
+++ test/t/test_ssh_keygen.py
@@ -6,3 +6,20 @@ class TestSshKeygen:
     @pytest.mark.complete("ssh-keygen -", require_cmd=True)
     def test_1(self, completion):
         assert completion
+
+    @pytest.mark.complete("ssh-keygen -s foo_key ssh-copy-id/.ssh/")
+    def test_filedir_pub_at_end_of_s(self, completion):
+        assert completion
+        assert all(x.endswith(".pub") for x in completion)
+
+    @pytest.mark.complete("ssh-keygen -s foo_key -n foo,")
+    def test_usernames_for_n(self, completion):
+        assert completion
+        assert not any("," in x for x in completion)
+        # TODO check that these are usernames
+
+    @pytest.mark.complete("ssh-keygen -s foo_key -h -n foo,")
+    def test_host_for_h_n(self, completion):
+        assert completion
+        assert not any("," in x for x in completion)
+        # TODO check that these are hostnames
