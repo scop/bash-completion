568c658c2c0ac6097f5332d4bfe23a9cf7b4de37
Ville Skyttä <ville.skytta@iki.fi>
2020-04-26 14:57:41 +0300

    crontab: fix loop over already given args
    Refs https://github.com/scop/bash-completion/issues/407

--- test/t/test_crontab.py
+++ test/t/test_crontab.py
@@ -5,3 +5,12 @@ class TestCrontab:
     @pytest.mark.complete("crontab ")
     def test_1(self, completion):
         assert completion
+
+    @pytest.mark.complete("crontab -l -")
+    def test_only_u_with_l(self, completion):
+        assert completion == "-u"
+
+    @pytest.mark.complete("crontab -r -")
+    def test_no_l_with_r(self, completion):
+        assert completion
+        assert "-l" not in completion
