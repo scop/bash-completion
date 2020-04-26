c41b772dcb4e31c8a6ee8d3052f3d2742d7a889d
Ville Skyttä <ville.skytta@iki.fi>
2020-04-26 14:28:48 +0300

    apt-cache: fix command mode handling
    Refs https://github.com/scop/bash-completion/issues/406

--- test/t/test_apt_cache.py
+++ test/t/test_apt_cache.py
@@ -5,9 +5,13 @@ import pytest
 class TestAptCache:
     @pytest.mark.complete("apt-cache ")
     def test_1(self, completion):
-        assert completion
+        assert "search" in completion
 
     @pytest.mark.complete("apt-cache showsrc [", require_cmd=True)
     def test_2(self, completion):
         # Doesn't actually fail on grep errors, but takes a long time.
+        assert not completion
+
+    @pytest.mark.complete("apt-cache ", trail=" add foo")
+    def test_special_at_point(self, completion):
         assert not completion
