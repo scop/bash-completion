53511617f0ab972d0b6912990ebf0adc3eeffe9d
Ville Skyttä <ville.skytta@iki.fi>
2020-05-04 21:00:38 +0300

    test: prefix fake test commands with underscore

--- test/t/test_umount.py
+++ test/t/test_umount.py
@@ -11,23 +11,25 @@ class TestUmount:
         (correctly) uses absolute paths. So we create a custom completion which
         reads from a file in our text fixture instead.
         """
-        assert_bash_exec(bash, "unset COMPREPLY cur; unset -f _mnt")
+        assert_bash_exec(bash, "unset COMPREPLY cur; unset -f _mnt_completion")
         assert_bash_exec(
             bash,
-            "_mnt() { "
+            "_mnt_completion() { "
             "local cur=$(_get_cword); "
             "_linux_fstab $(_get_pword) < mount/test-fstab; "
-            "} && complete -F _mnt mnt",
+            "} && complete -F _mnt_completion _mnt",
         )
         request.addfinalizer(
-            lambda: assert_bash_exec(bash, "complete -r mnt; unset -f _mnt")
+            lambda: assert_bash_exec(
+                bash, "complete -r _mnt; unset -f _mnt_completion"
+            )
         )
 
     @pytest.mark.complete("umount ")
     def test_1(self, completion):
         assert completion
 
-    @pytest.mark.complete("mnt /mnt/nice-test-p")
+    @pytest.mark.complete("_mnt /mnt/nice-test-p")
     def test_mnt_basic(self, completion, dummy_mnt):
         assert completion == "/mnt/nice-test-path"
 
@@ -35,40 +37,40 @@ class TestUmount:
     # is shell unescaped due to how assert_complete handles the
     # "one result on same line case".
 
-    @pytest.mark.complete(r"mnt /mnt/nice\ test-p")
+    @pytest.mark.complete(r"_mnt /mnt/nice\ test-p")
     def test_mnt_space(self, completion, dummy_mnt):
         assert completion == r"/mnt/nice test-path"
 
-    @pytest.mark.complete(r"mnt /mnt/nice\$test-p")
+    @pytest.mark.complete(r"_mnt /mnt/nice\$test-p")
     def test_mnt_dollar(self, completion, dummy_mnt):
         assert completion == "/mnt/nice$test-path"
 
-    @pytest.mark.complete(r"mnt /mnt/nice\ test\\p")
+    @pytest.mark.complete(r"_mnt /mnt/nice\ test\\p")
     def test_mnt_backslash(self, completion, dummy_mnt):
         assert completion == r"/mnt/nice test\path"
 
-    @pytest.mark.complete(r"mnt /mnt/nice\ ")
+    @pytest.mark.complete(r"_mnt /mnt/nice\ ")
     def test_mnt_after_space(self, completion, dummy_mnt):
         assert completion == sorted(
             (r"/mnt/nice\ test\\path", r"/mnt/nice\ test-path")
         )
 
-    @pytest.mark.complete(r"mnt /mnt/nice\$")
+    @pytest.mark.complete(r"_mnt /mnt/nice\$")
     def test_mnt_at_dollar(self, completion, dummy_mnt):
         assert completion == "/mnt/nice$test-path"
 
-    @pytest.mark.complete(r"mnt /mnt/nice\'")
+    @pytest.mark.complete(r"_mnt /mnt/nice\'")
     def test_mnt_at_quote(self, completion, dummy_mnt):
         assert completion == "/mnt/nice'test-path"
 
-    @pytest.mark.complete("mnt /mnt/other")
+    @pytest.mark.complete("_mnt /mnt/other")
     def test_mnt_other(self, completion, dummy_mnt):
         assert completion == "/mnt/other'test path"
 
-    @pytest.mark.complete("mnt -L Ubu")
+    @pytest.mark.complete("_mnt -L Ubu")
     def test_mnt_label_space(self, completion, dummy_mnt):
         assert completion == "Ubuntu Karmic"
 
-    @pytest.mark.complete("mnt -L Deb")
+    @pytest.mark.complete("_mnt -L Deb")
     def test_mnt_label_quote(self, completion, dummy_mnt):
         assert completion == "Debian-it's awesome"
