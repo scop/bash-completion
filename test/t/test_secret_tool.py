e9a556f4a233250c6f05dfd0c5e9ae17a0ffd747
Ville Skyttä <ville.skytta@iki.fi>
2020-05-08 21:37:16 +0300

    secret-tool: new completion

---
+++ test/t/test_secret_tool.py
@@ -0,0 +1,12 @@
+import pytest
+
+
+@pytest.mark.bashcomp(cmd="secret-tool",)
+class TestSecretTool:
+    @pytest.mark.complete("secret-tool ", require_cmd=True)
+    def test_modes(self, completion):
+        assert "store" in completion
+
+    @pytest.mark.complete("secret-tool search ")
+    def test_no_complete(self, completion):
+        assert not completion
