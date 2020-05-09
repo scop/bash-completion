c198a94029aa33cfbd4bd0586e1674859337c35c
Ville Skyttä <ville.skytta@iki.fi>
2020-05-09 23:30:49 +0300

    lftp: use "bookmark list" command to list bookmarks
    Closes https://github.com/scop/bash-completion/issues/412

--- test/t/test_lftp.py
+++ test/t/test_lftp.py
@@ -3,11 +3,13 @@ import pytest
 
 @pytest.mark.bashcomp(pre_cmds=("HOME=$PWD/lftp",))
 class TestLftp:
-    @pytest.mark.complete("lftp ")
+    @pytest.mark.complete("lftp ", require_cmd=True)
     def test_1(self, bash, completion, output_sort_uniq):
         hosts = output_sort_uniq("compgen -A hostname")
         assert all(x in completion for x in hosts)
-        assert "lftptest" in completion  # defined in lftp/.lftp/bookmarks
+        # defined in lftp/.lftp/bookmarks
+        assert all(x in completion for x in "lftptest spacetest".split())
+        assert "badbookmark" not in completion
 
     @pytest.mark.complete("lftp -", require_cmd=True)
     def test_2(self, completion):
