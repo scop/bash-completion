62b3dc6dc2024eb2101bddcd468552d485c60a8c
Ville Skyttä <ville.skytta@iki.fi>
2020-05-09 21:49:07 +0300

    test/slapt-src: convert remaining test case to pytest+pexpect

--- test/t/test_slapt_src.py
+++ test/t/test_slapt_src.py
@@ -1,8 +1,27 @@
+import os
+from tempfile import mkstemp
+
 import pytest
+
+from conftest import assert_complete, is_bash_type
 
 
 @pytest.mark.bashcomp(cmd="slapt-src")
 class TestSlaptSrc:
+    @pytest.fixture(scope="class")
+    def slapt_srcrc(self, request, bash):
+        fd, fname = mkstemp(prefix="slapt-srcrc.", text=True)
+        request.addfinalizer(lambda: os.remove(fname))
+        with os.fdopen(fd, "w") as f:
+            print(
+                "BUILDDIR=%s/"
+                % os.path.join(
+                    bash.cwd, *"slackware usr src slapt-src".split()
+                ),
+                file=f,
+            )
+        return fname
+
     @pytest.mark.complete("slapt-src -", require_cmd=True)
     def test_1(self, completion):
         assert completion
@@ -14,3 +33,11 @@ class TestSlaptSrc:
     @pytest.mark.complete("slapt-src --ins", require_cmd=True)
     def test_3(self, completion):
         assert completion == "--install"
+
+    def test_install(self, bash, slapt_srcrc):
+        if not is_bash_type(bash, "slapt-src"):
+            pytest.skip("slapt-src not found")
+        completion = assert_complete(
+            bash, "slapt-src --config %s --install " % slapt_srcrc
+        )
+        assert completion == "abc:4 qwe:2.1".split()
