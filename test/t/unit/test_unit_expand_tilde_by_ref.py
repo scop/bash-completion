d258b3ca8ab65fec4dfc6a14c4697212e09ad69c
Ville Skyttä <ville.skytta@iki.fi>
2020-05-10 10:19:55 +0300

    test/__expand_tilde_by_ref: port remaining test cases to pytest+pexpect

--- test/t/unit/test_unit_expand_tilde_by_ref.py
+++ test/t/unit/test_unit_expand_tilde_by_ref.py
@@ -3,8 +3,16 @@ import pytest
 from conftest import assert_bash_exec
 
 
-@pytest.mark.bashcomp(cmd=None)
+@pytest.mark.bashcomp(cmd=None, ignore_env=r"^[+-]var=")
 class TestUnitExpandTildeByRef:
+    @pytest.fixture(scope="class")
+    def user_home(self, bash):
+        user = assert_bash_exec(
+            bash, 'id -un 2>/dev/null || echo "$USER"', want_output=True
+        ).strip()
+        home = assert_bash_exec(bash, 'echo "$HOME"', want_output=True).strip()
+        return (user, home)
+
     def test_1(self, bash):
         assert_bash_exec(bash, "__expand_tilde_by_ref >/dev/null")
 
@@ -14,3 +22,33 @@ class TestUnitExpandTildeByRef:
             bash,
             '_x() { local aa="~"; __expand_tilde_by_ref aa; }; _x; unset _x',
         )
+
+    @pytest.mark.parametrize("plain_tilde", (True, False))
+    @pytest.mark.parametrize(
+        "suffix_expanded",
+        (
+            ("", True),
+            ("/foo", True),
+            (r"/\$HOME", True),
+            ("/a  b", True),
+            ("/*", True),
+            (";echo hello", False),
+            ("/a;echo hello", True),
+        ),
+    )
+    def test_expand(self, bash, user_home, plain_tilde, suffix_expanded):
+        user, home = user_home
+        suffix, expanded = suffix_expanded
+        if plain_tilde:
+            user = ""
+            if not suffix or not expanded:
+                home = "~"
+        elif not expanded:
+            home = "~%s" % user
+        output = assert_bash_exec(
+            bash,
+            r'var="~%s%s"; __expand_tilde_by_ref var; printf "%%s\n" "$var"'
+            % (user, suffix),
+            want_output=True,
+        )
+        assert output.strip() == "%s%s" % (home, suffix.replace(r"\$", "$"),)
