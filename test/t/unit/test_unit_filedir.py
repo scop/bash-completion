54c732a82066e1595d8dd153e992987ea87b54b6
Ville Skyttä <ville.skytta@iki.fi>
2020-05-10 12:37:45 +0300

    test/_filedir: port more test cases to pytest+pexpect

--- test/t/unit/test_unit_filedir.py
+++ test/t/unit/test_unit_filedir.py
@@ -123,3 +123,21 @@ class TestUnitFiledir:
     @pytest.mark.complete("g ", cwd="_filedir/ext")
     def test_16(self, functions, completion):
         assert completion == sorted("ee.e1 foo/ gg.e1 ii.E1".split())
+
+    @pytest.mark.parametrize("funcname", "f f2".split())
+    def test_17(self, bash, functions, funcname):
+        completion = assert_complete(
+            bash, r"%s a\$b/" % funcname, cwd="_filedir"
+        )
+        # endswith: in CentOS 6 container we get something like this, accept it:
+        #     a\$b/\x08\x08\x08\x08\x08/work/test/fixtures/_filedir/a\$b/h
+        assert completion == r"a$b/h" or completion.output.strip().endswith(
+            "\b\b\b\b%s/_filedir/a\\$b/h" % bash.cwd
+        )
+
+    @pytest.mark.parametrize("funcname", "f f2".split())
+    def test_18(self, bash, functions, funcname):
+        completion = assert_complete(
+            bash, r"%s \[x" % funcname, cwd="_filedir/brackets"
+        )
+        assert completion == r"[x]"
