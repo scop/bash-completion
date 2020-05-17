7aea619b1b4563f6a5a55877aeeea009e71cfd87
Ville Skyttä <ville.skytta@iki.fi>
2020-05-17 22:40:00 +0300

    postfix: try to arrange a fake tty so we can tickle the usage message out

--- test/t/test_postfix.py
+++ test/t/test_postfix.py
@@ -1,3 +1,5 @@
+import getpass
+
 import pytest
 
 
@@ -6,7 +8,15 @@ class TestPostfix:
     def test_1(self, completion):
         assert completion
 
-    @pytest.mark.xfail  # see TODO in completion
-    @pytest.mark.complete("postfix -", require_cmd=True)
-    def test_2(self, completion):
+    @pytest.mark.xfail(
+        getpass.getuser() != "root",
+        reason="Likely outputs usage only for root",
+    )
+    @pytest.mark.complete(
+        "postfix -",
+        require_cmd=True,
+        xfail="! type unbuffer &>/dev/null",
+        sleep_after_tab=2,  # postfix is slow to output usage
+    )
+    def test_options(self, completion):
         assert completion
