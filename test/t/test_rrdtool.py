b06a35a4658e22e580dfda4bf1a28d7d55c1bbfa
Ben Weir <ben@scurvy.net>
2020-07-13 22:41:08 -0700

    rrdtool: redo completions to ignore options. also handle dump

--- test/t/test_rrdtool.py
+++ test/t/test_rrdtool.py
@@ -6,21 +6,47 @@ class TestRrdtool:
     def test_1(self, completion):
         assert completion
 
-    @pytest.mark.complete("rrdtool dump rrdtool/")
-    def test_dump(self, completion):
+    @pytest.mark.complete("rrdtool info rrdtool/")
+    def test_info_first_arg(self, completion):
         assert completion
         assert all(x.endswith(".rrd") for x in completion)
 
-    @pytest.mark.complete("rrdtool restore rrdtool/")
-    def test_restore(self, completion):
+    @pytest.mark.complete("rrdtool info rrdtool/test.rrd")
+    def test_info_with_all_args(self, completion):
+        assert not completion
+
+    @pytest.mark.complete("rrdtool dump rrdtool/")
+    def test_dump_first_arg(self, completion):
+        assert completion
+        assert all(x.endswith(".rrd") for x in completion)
+
+    @pytest.mark.complete("rrdtool dump rrdtool/test.rrd rrdtool/")
+    def test_dump_second_arg(self, completion):
         assert completion
         assert all(x.endswith(".xml") for x in completion)
 
-    @pytest.mark.complete("rrdtool restore --force rrdtool/")
-    def test_restore_with_option(self, completion):
+    @pytest.mark.complete("rrdtool restore rrdtool/")
+    def test_restore_first_arg(self, completion):
         assert completion
         assert all(x.endswith(".xml") for x in completion)
 
     @pytest.mark.complete("rrdtool restore rrdtool/test.xml")
-    def test_restore_with_xml(self, completion):
+    def test_restore_second_arg(self, completion):
+        assert all(x.endswith(".rrd") for x in completion)
+
+    @pytest.mark.complete("rrdtool restore --force rrdtool/")
+    def test_restore_with_leading_option(self, completion):
+        assert completion
+        assert all(x.endswith(".xml") for x in completion)
+
+    @pytest.mark.complete("rrdtool restore rrdtool/test.xml -f")
+    def test_restore_second_arg_with_trailing_option(self, completion):
+        assert all(x.endswith(".rrd") for x in completion)
+
+    @pytest.mark.complete("rrdtool restore rrdtool/test.xml rrdtool/test.rrd")
+    def test_restore_with_all_args(self, completion):
+        assert not completion
+
+    @pytest.mark.complete("rrdtool restore rrdtool/test.xml rrdtool/test.rrd -f")
+    def test_restore_with_all_args_and_option(self, completion):
         assert not completion
