bf3f72046f62ea86f43b65389d6d706a28544adb
Ville Skyttä <ville.skytta@iki.fi>
2020-07-25 10:57:04 +0300

    pytest: rewrite in bash, support toplevel funcs, avoid nondef ones and classes

---
+++ test/fixtures/pytest/test_async.py
@@ -0,0 +1,17 @@
+"""Async function pytest completion fixture."""
+
+
+async def test_positive():
+    pass
+
+
+async def non_test_negative():
+    pass
+
+
+class Testing:
+    async def test_positive(self):
+        pass
+
+    async def non_test_negative(self):
+        pass
--- test/t/test_pytest.py
+++ test/t/test_pytest.py
@@ -13,11 +13,38 @@ class TestPytest:
         assert completion
 
     @pytest.mark.complete("pytest ../t/test_pytest.py:")
-    def test_classes(self, completion):
-        assert completion == ":TestPytest"
+    def test_classes_and_functions(self, completion):
+        assert completion == ":TestPytest :test_function_canary".split()
 
     @pytest.mark.complete("pytest ../t/test_pytest.py::TestPytest::")
     def test_class_methods(self, completion):
-        methods = inspect.getmembers(self, predicate=inspect.ismethod)
-        assert len(completion) == len(methods)
-        assert completion == [x[0] for x in methods]
+        methods = [
+            x[0]
+            for x in inspect.getmembers(self, predicate=inspect.ismethod)
+            if x[0].startswith("test_")
+        ]
+        assert completion == methods
+
+    @pytest.mark.complete("pytest pytest/test_async.py:")
+    def test_classes_and_async_functions(self, completion):
+        assert completion == ":Testing :test_positive".split()
+
+    @pytest.mark.complete("pytest pytest/test_async.py::Testing::")
+    def test_async_class_methods(self, completion):
+        assert completion == "test_positive"
+
+    def non_test_cananary_method(self):
+        pass
+
+
+def test_function_canary():
+    pass
+
+
+def non_test_canary():
+    pass
+
+
+class NonTestCanaryClass:
+    def test_is_this_function_not(self):
+        pass
