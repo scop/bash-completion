5961e2e87dcef2d84df2fb4fe732584f7bcae69b
relrelb <relrelb@users.noreply.github.com>
2020-09-17 23:11:40 +0300

    ps: new completion (#435)
    Closes https://github.com/scop/bash-completion/issues/434
    
    Co-authored-by: Ville Skyttä <ville.skytta@iki.fi>

---
+++ test/t/test_ps.py
@@ -0,0 +1,50 @@
+import pytest
+
+
+def is_int(s):
+    try:
+        int(s)
+    except ValueError:
+        return False
+    else:
+        return True
+
+
+class TestPs:
+    @pytest.mark.complete("ps -")
+    def test_1(self, completion):
+        assert completion
+
+    @pytest.mark.complete("ps --help ")
+    def test_2(self, completion):
+        assert completion == [
+            "all",
+            "list",
+            "misc",
+            "output",
+            "simple",
+            "threads",
+        ]
+
+    @pytest.mark.complete("ps --help all ")
+    def test_3(self, completion):
+        assert not completion
+
+    @pytest.mark.complete("ps --version ")
+    def test_4(self, completion):
+        assert not completion
+
+    @pytest.mark.complete("ps --pid ")
+    def test_5(self, completion):
+        assert completion
+        assert all(map(is_int, completion))
+
+    @pytest.mark.complete("ps --format ")
+    def test_6(self, completion):
+        assert completion
+        assert all(map(lambda c: not c.startswith(("-", ",")), completion))
+
+    @pytest.mark.complete("ps --format user,")
+    def test_7(self, completion):
+        assert completion
+        assert all(map(lambda c: not c.startswith(("-", ",")), completion))
