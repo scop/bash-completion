d7f9401e9e5976b92ebca9517efd308d6680e91f
JCGoran <JCGoran@users.noreply.github.com>
2020-11-07 12:06:19 +0100

    apt-mark: new completion (#398)
    Co-authored-by: Ville Skyttä <ville.skytta@iki.fi>
    Co-authored-by: Alcolo47 <Alcolo47@gmail.com>

---
+++ test/t/test_apt_mark.py
@@ -0,0 +1,34 @@
+import pytest
+
+
+@pytest.mark.bashcomp(cmd="apt-mark")
+class TestAptMark:
+    @pytest.mark.complete("apt-mark ")
+    def test_1(self, completion):
+        assert all(
+            x in completion
+            for x in (
+                "auto manual remove showinstall showremove "
+                "hold minimize-manual showauto showmanual unhold install "
+                "purge showhold showpurge"
+            ).split()
+        )
+
+    @pytest.mark.complete("apt-mark minimize-manual ")
+    def test_2(self, completion):
+        assert not completion
+
+    @pytest.mark.complete("apt-mark --file=", cwd="dpkg")
+    def test_3(self, completion):
+        assert (
+            completion
+            == "bash-completion-test-nonsubject.txt bash-completion-test-subject.deb".split()
+        )
+
+    @pytest.mark.complete("apt-mark --config-file ", cwd="apt-mark")
+    def test_4(self, completion):
+        assert completion == "example.conf"
+
+    @pytest.mark.complete("apt-mark --option ")
+    def test_5(self, completion):
+        assert not completion
