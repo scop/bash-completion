48090fce97b8d84f90eebb813ca64c24ef023fb4
Ville Skyttä <ville.skytta@iki.fi>
2020-12-12 23:56:00 +0200

    md5sum,sha{,1,224,256,384,512}sum: take -c/--check into account for filenames

---
+++ test/t/test_sha256sum.py
@@ -0,0 +1,15 @@
+import pytest
+
+
+class TestSha256sum:
+    @pytest.mark.complete("sha256sum -", require_cmd=True)
+    def test_options(self, completion):
+        assert completion
+
+    @pytest.mark.complete("sha256sum ", cwd="sha256sum")
+    def test_summing(self, completion):
+        assert completion == "foo"
+
+    @pytest.mark.complete("sha256sum -c ", cwd="sha256sum")
+    def test_checking(self, completion):
+        assert completion == "foo.sha256"
