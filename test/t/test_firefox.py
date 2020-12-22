3bed0ddbe0aec8e4840ebe8cb3c78bd7568f3b2d
Ville Skyttä <ville.skytta@iki.fi>
2020-12-22 22:06:53 +0200

    test/firefox: xfail options completion on --help failure
    For example, running as root in GH actions container croaks:
      Running Firefox as root in a regular user's session is not supported.
      ($HOME is /github/home which is owned by uid 1001.)

--- test/t/test_firefox.py
+++ test/t/test_firefox.py
@@ -6,7 +6,12 @@ class TestFirefox:
     def test_1(self, completion):
         assert completion
 
-    @pytest.mark.complete("firefox -", require_cmd=True)
+    # --help test: running as root in GH actions container croaks:
+    #     Running Firefox as root in a regular user's session is not supported.
+    #     ($HOME is /github/home which is owned by uid 1001.)
+    @pytest.mark.complete(
+        "firefox -", require_cmd=True, xfail="! firefox --help &>/dev/null"
+    )
     def test_2(self, completion):
         assert completion
         assert not completion.endswith(" ")
