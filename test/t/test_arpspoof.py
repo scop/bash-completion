b767c4b577af615b5522e064ec807f0b29f97575
Ville Skyttä <ville.skytta@iki.fi>
2020-12-23 22:54:38 +0200

    test/arpspoof: skip gracefully on libnet_select_device errors
    With docker --network none, Debian 10:
    
        # arpspoof --help
        arpspoof: libnet_select_device(): no network interface found

--- test/t/test_arpspoof.py
+++ test/t/test_arpspoof.py
@@ -5,8 +5,13 @@ class TestArpspoof:
     @pytest.mark.complete(
         "arpspoof -",
         require_cmd=True,
-        # May require privileges even for outputting the usage message
-        skipif="arpspoof 2>&1 | command grep -qF libnet_open_link",
+        # May require privileges or network interfaces available even for
+        # outputting the usage message. Unfortunately --help provokes a
+        # non-zero exit status so we cannot test for that.
+        skipif=(
+            "arpspoof 2>&1 | "
+            "command grep -qE 'libnet_(open_link|select_device)'"
+        ),
     )
     def test_1(self, completion):
         assert completion
