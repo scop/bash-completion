bae3a6e01ca81051be943fb84f133a016af247f2
Ville Skyttä <ville.skytta@iki.fi>
2021-02-20 17:49:18 +0200

    feat(pdftoppm): new completion

---
+++ test/t/test_pdftoppm.py
@@ -0,0 +1,11 @@
+import pytest
+
+
+class TestPdftoppm:
+    @pytest.mark.complete("pdftoppm ")
+    def test_files(self, completion):
+        assert completion
+
+    @pytest.mark.complete("pdftoppm -")
+    def test_options(self, completion):
+        assert completion
