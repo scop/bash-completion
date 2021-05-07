ccf7bf69d9572bd44efd7c4600a17b5d8abeed20
Ville Skyttä <ville.skytta@iki.fi>
2021-05-07 21:23:06 +0300

    test(dmesg,vipw): expect no options on macOS

--- test/t/test_vipw.py
+++ test/t/test_vipw.py
@@ -1,7 +1,12 @@
+import sys
+
 import pytest
 
 
 class TestVipw:
     @pytest.mark.complete("vipw -", require_cmd=True)
     def test_1(self, completion):
-        assert completion
+        if sys.platform == "darwin":
+            assert not completion  # takes no options
+        else:
+            assert completion
