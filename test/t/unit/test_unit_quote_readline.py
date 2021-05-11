8d55324af4b7213ac472019d8bb417020be86391
Koichi Murase <myoga.murase@gmail.com>
2021-05-12 05:33:51 +0900

    test(quote_readline): use temporary directories

--- test/t/unit/test_unit_quote_readline.py
+++ test/t/unit/test_unit_quote_readline.py
@@ -1,5 +1,7 @@
 import pytest
 import os
+import tempfile
+import re
 
 from conftest import assert_bash_exec
 
@@ -14,6 +16,13 @@ class TestUnitQuoteReadline:
         assert_bash_exec(
             bash, "foo() { quote_readline meh >/dev/null; }; foo; unset foo"
         )
+
+    def quote_bash_word(self, s):
+        return "$'" + re.sub(
+            r'[\\\'\000-\037]',
+            lambda m: r'\\' if m.group(0) == '\\'
+            else r'\'' if m.group(0) == "'"
+            else r'\%03o' % ord(m.group(0)), s) + "'"
 
     def test_github_issue_189_1(self, bash):
         """Test error messages on a certain command line
@@ -42,12 +51,9 @@ class TestUnitQuoteReadline:
           $ echo '$(touch file.txt)[TAB]
 
         """
-        if os.path.exists("1.txt"):
-            os.remove("1.txt")
-        assert_bash_exec(bash, "quote_readline $'\\'$(touch 1.txt)' >/dev/null")
-        if os.path.exists("1.txt"):
-            os.remove("1.txt")
-            assert False
+        with tempfile.TemporaryDirectory() as tmpdir:
+            assert_bash_exec(bash, "(cd %s; quote_readline $'\\'$(touch 1.txt)' >/dev/null)" % self.quote_bash_word(tmpdir))
+            assert not os.path.exists(os.path.join(tmpdir, "1.txt"))
 
     def test_github_issue_492_2(self, bash):
         """Test the file clear by unintended redirection on a certain command line
@@ -60,12 +66,9 @@ class TestUnitQuoteReadline:
           $ awk '$1 > 1.0[TAB]
         
         """
-        if os.path.exists("1.0"):
-            os.remove("1.0")
-        assert_bash_exec(bash, "quote_readline $'\\'$1 > 1.0' >/dev/null")
-        if os.path.exists("1.0"):
-            os.remove("1.0")
-            assert False
+        with tempfile.TemporaryDirectory() as tmpdir:
+            assert_bash_exec(bash, "(cd %s; quote_readline $'\\'$1 > 1.0' >/dev/null)" % self.quote_bash_word(tmpdir))
+            assert not os.path.exists(os.path.join(tmpdir, "1.0"))
 
     def test_github_issue_492_3(self, bash):
         """Test code execution through unintended pathname expansions
@@ -77,12 +80,9 @@ class TestUnitQuoteReadline:
           $ echo '$*[TAB]
     
         """
-        tmpfile = "ret=$(echo injected >&2)"
-        with open(tmpfile, 'a'):
-            os.utime(tmpfile, None)
-        assert_bash_exec(bash, "quote_readline $'\\'$*' >/dev/null")
-        if os.path.exists(tmpfile):
-            os.remove(tmpfile)
+        with tempfile.TemporaryDirectory() as tmpdir:
+            os.mkdir(os.path.join(tmpdir, "ret=$(echo injected >&2)"))
+            assert_bash_exec(bash, "(cd %s; quote_readline $'\\'$*' >/dev/null)" % self.quote_bash_word(tmpdir))
 
     def test_github_issue_492_4(self, bash):
         """Test error messages through unintended pathname expansions
@@ -92,7 +92,8 @@ class TestUnitQuoteReadline:
         the failure of pathname expansions.
         
           $ shopt -s failglob
-          $ echo a\	b*[TAB]
+          $ echo a\\	b*[TAB]
         
         """
-        assert_bash_exec(bash, "(shopt -s failglob;quote_readline $'a\\\\\\tb*' >/dev/null)")
+        with tempfile.TemporaryDirectory() as tmpdir:
+            assert_bash_exec(bash, "(cd %s; shopt -s failglob; quote_readline $'a\\\\\\tb*' >/dev/null)" % self.quote_bash_word(tmpdir))
