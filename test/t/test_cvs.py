39558a064a5ef853a4bfe97b1fe4978bc9f34439
Keith Carangelo <keithcarangelo@safetyinsurance.com>
2021-05-13 11:39:04 -0400

    Added support for 'cvs status' completions

--- test/t/test_cvs.py
+++ test/t/test_cvs.py
@@ -18,3 +18,24 @@ class TestCvs:
     @pytest.mark.complete("cvs -", require_cmd=True)
     def test_4(self, completion):
         assert completion
+
+    @pytest.mark.complete("cvs update -AdP foo/", cwd="cvs")
+    def test_5(self, completion):
+        assert completion == "bar"
+
+    @pytest.mark.complete("cvs log -v foo/", cwd="cvs")
+    def test_6(self, completion):
+        assert completion == "bar"
+
+    @pytest.mark.complete("cvs diff foo/", cwd="cvs")
+    def test_7(self, completion):
+        assert completion == "bar"
+
+    @pytest.mark.complete("cvs status -v foo/", cwd="cvs")
+    def test_8(self, completion):
+        assert completion == "bar"
+
+    @pytest.mark.complete("cvs status foo/", cwd="cvs")
+    def test_9(self, completion):
+        assert completion == "bar"
+
