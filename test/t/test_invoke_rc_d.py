6b2ae7c19c4b2a9a626125822aa52cec42f1bf0f
Ville Skyttä <ville.skytta@iki.fi>
2021-05-21 22:19:27 +0300

    test(invoke-rc.d,service): xfail service completion on macOS
    We don't have that implemented currently.

--- test/t/test_invoke_rc_d.py
+++ test/t/test_invoke_rc_d.py
@@ -1,8 +1,14 @@
+import sys
+
 import pytest
 
 
 @pytest.mark.bashcomp(cmd="invoke-rc.d")
 class TestInvokeRcD:
+    @pytest.mark.xfail(
+        sys.platform == "darwin",
+        reason="Service completion not available on macOS",
+    )
     @pytest.mark.complete("invoke-rc.d ")
     def test_1(self, completion):
         assert completion
@@ -10,5 +16,6 @@ class TestInvokeRcD:
     @pytest.mark.complete("invoke-rc.d --no-fallback --")
     def test_2(self, completion):
         """Test already specified option is not offered."""
-        assert completion
+        if sys.platform != "darwin":  # no service completion
+            assert completion
         assert "--no-fallback" not in completion
--- test/t/test_service.py
+++ test/t/test_service.py
@@ -1,7 +1,13 @@
+import sys
+
 import pytest
 
 
 class TestService:
+    @pytest.mark.xfail(
+        sys.platform == "darwin",
+        reason="Service completion not available on macOS",
+    )
     @pytest.mark.complete("service ")
     def test_1(self, completion):
         assert completion
