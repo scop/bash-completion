db2e9efa0469aee76507631fddef93c7446dd120
Ville Skyttä <ville.skytta@iki.fi>
2021-05-21 21:53:47 +0300

    test(javaws): xfail options test on unparseable -help
    macOS in GitHub workflows barfs:
    
        No Java runtime present, requesting install.
        Unable to locate a Java Runtime to invoke.
    
    The exit code of -help is nonzero, so resort to grep to try and detect
    this.

--- test/t/test_javaws.py
+++ test/t/test_javaws.py
@@ -6,6 +6,12 @@ class TestJavaws:
     def test_1(self, completion):
         assert completion
 
-    @pytest.mark.complete("javaws -", require_cmd=True)
+    @pytest.mark.complete(
+        "javaws -",
+        require_cmd=True,
+        xfail=(
+            "! (javaws -help 2>&1 || :) | " "command grep -q -- '[[:space:]]-'"
+        ),
+    )
     def test_2(self, completion):
         assert completion
