e8fe9ba5df5f16ea78ddad5edde277a36d391dbc
Ville Skyttä <ville.skytta@iki.fi>
2021-05-27 23:29:55 +0300

    test(ant): extract complete-ant-cmd.pl presence to a fixture
    For reusability.

--- test/t/test_ant.py
+++ test/t/test_ant.py
@@ -9,6 +9,11 @@ from conftest import assert_bash_exec
     pre_cmds=('cp "$SRCDIRABS"/fixtures/ant/*.xml .',),
 )
 class TestAnt:
+    @pytest.fixture(scope="class")
+    def has_complete_ant_cmd_pl(self, bash):
+        output = assert_bash_exec(bash, "complete -p ant", want_output=True)
+        return "complete-ant-cmd.pl" in output
+
     @pytest.mark.complete("ant -", require_cmd=True)
     def test_1(self, completion):
         assert completion
@@ -22,9 +27,8 @@ class TestAnt:
         assert completion == "build-with-import imported-build".split()
 
     @pytest.mark.complete("ant ", env=dict(ANT_ARGS="'-f named-build.xml'"))
-    def test_4(self, bash, completion):
-        output = assert_bash_exec(bash, "complete -p ant", want_output=True)
-        if "complete-ant-cmd.pl" in output:
+    def test_4(self, bash, completion, has_complete_ant_cmd_pl):
+        if has_complete_ant_cmd_pl:
             # Some versions of complete-ant-cmd.pl don't treat ANT_ARGS right;
             # in those cases we get the correct completion produced by _ant
             # plus whatever complete-ant-cmd.pl was able to get from build.xml
