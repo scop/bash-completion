f36d28a90e91ddaf24c274439e8eb8863091801d
Ville Skyttä <ville.skytta@iki.fi>
2021-06-11 22:42:15 +0300

    feat(lrzip): complete long option args

--- test/t/test_lrzip.py
+++ test/t/test_lrzip.py
@@ -13,3 +13,12 @@ class TestLrzip:
     @pytest.mark.complete("lrzip -", require_cmd=True)
     def test_3(self, completion):
         assert completion
+
+    @pytest.mark.complete(
+        "lrzip --",
+        # require_longopt not applicable, useful --help may give nonzero exit
+        require_cmd=True,
+        xfail=("! { lrzip --help 2>&1 || :; } | command grep -qF -- --help"),
+    )
+    def test_longopt(self, completion):
+        assert completion
