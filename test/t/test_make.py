64630aaeca0a5013e4b438577138f0f707c68cb1
Koichi Murase <myoga.murase@gmail.com>
2021-06-20 11:00:54 +0900

    test(make): add test cases for GitHub #544

--- test/t/test_make.py
+++ test/t/test_make.py
@@ -2,6 +2,8 @@ import os
 
 import pytest
 
+from conftest import assert_complete
+
 
 class TestMake:
     @pytest.mark.complete("make -f Ma", cwd="make")
@@ -45,3 +47,38 @@ class TestMake:
     @pytest.mark.complete("make -", require_cmd=True)
     def test_9(self, completion):
         assert completion
+
+
+@pytest.mark.bashcomp(require_cmd=True, cwd="make/test2")
+class TestMake2:
+    def test_github_issue_544_1(self, bash):
+        completion = assert_complete(bash, "make ab")
+        assert completion == "c/xyz"
+
+    def test_github_issue_544_2(self, bash):
+        completion = assert_complete(bash, "make 1")
+        assert completion == "23/"
+
+    def test_github_issue_544_3(self, bash):
+        completion = assert_complete(bash, "make 123/")
+        assert completion == ["xaa", "xbb"]
+
+    def test_github_issue_544_4(self, bash):
+        completion = assert_complete(bash, "make 123/xa")
+        assert completion == "a"
+
+    def test_subdir_1(self, bash):
+        completion = assert_complete(bash, "make sub1")
+        assert completion == "test/bar/"
+
+    def test_subdir_2(self, bash):
+        completion = assert_complete(bash, "make sub2")
+        assert completion == "test/bar/alpha"
+
+    def test_subdir_3(self, bash):
+        completion = assert_complete(bash, "make sub3")
+        assert completion == "test/"
+
+    def test_subdir_4(self, bash):
+        completion = assert_complete(bash, "make sub4")
+        assert completion == "sub4test/bar/ sub4test2/foo/gamma".split()
