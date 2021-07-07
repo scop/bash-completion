3e55711e3929f7a762a9e108858e77b4f34745f4
Ville Skyttä <ville.skytta@iki.fi>
2021-07-07 21:05:01 +0300

    feat(xmlwf): add -a and -b arg non-completions
    $ xmlwf -v
    xmlwf using expat_2.4.1
    [...]
    $ xmlwf --help
    [...]
      -a FACTOR     set maximum tolerated [a]mplification factor [...]
      -b BYTES      set number of output [b]ytes needed to activate [...]
    [...]
    
    Bundle a non-arg option in related test to test bundling behavior while
    at it.

--- test/t/test_xmlwf.py
+++ test/t/test_xmlwf.py
@@ -9,3 +9,7 @@ class TestXmlwf:
     @pytest.mark.complete("xmlwf -", require_cmd=True)
     def test_2(self, completion):
         assert completion
+
+    @pytest.mark.complete("xmlwf -sa ")
+    def test_no_arg_to_a(self, completion):
+        assert not completion
