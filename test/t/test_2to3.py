074ff4cad01e62f636a179a758d2854c895b16fb
Ville Skyttä <ville.skytta@iki.fi>
2021-07-31 10:54:03 +0300

    style(test): remove redundant require_cmd with require_longopt
    `require_longopt=True` implies `require_cmd=True` by default.

--- test/t/test_2to3.py
+++ test/t/test_2to3.py
@@ -6,6 +6,6 @@ class Test2to3:
     def test_1(self, completion):
         assert completion
 
-    @pytest.mark.complete("2to3 -", require_cmd=True, require_longopt=True)
+    @pytest.mark.complete("2to3 -", require_longopt=True)
     def test_2(self, completion):
         assert completion
--- test/t/test_pydocstyle.py
+++ test/t/test_pydocstyle.py
@@ -6,8 +6,6 @@ class TestPydocstyle:
     def test_1(self, completion):
         assert completion
 
-    @pytest.mark.complete(
-        "pydocstyle -", require_cmd=True, require_longopt=True
-    )
+    @pytest.mark.complete("pydocstyle -", require_longopt=True)
     def test_2(self, completion):
         assert completion
