75a38cabc7142494fe2a4ae9ffc986124a94ef8f
Ville Skyttä <ville.skytta@iki.fi>
2021-07-31 11:06:28 +0300

    feat(pylint): message ids completion
    Complete message ids for `--fail-on`, `--help-msg`, `--enable`, and
    `--disable`. We scrape the output of `--list-msgs-enabled` instead of
    `--list-msgs`, because it outputs both enabled and disable message ids
    and thus allows for one implementation for all cases, even though the
    output is somewhat less parseable than that of the latter.
    
    Curiously, `--list-msgs-enabled` also outputs some message ids that
    `--list-msgs` does not.

--- test/t/test_pylint.py
+++ test/t/test_pylint.py
@@ -9,3 +9,15 @@ class TestPylint:
     @pytest.mark.complete("pylint --confidence=HIGH,")
     def test_2(self, completion):
         assert completion
+
+    @pytest.mark.complete("pylint --help-msg=", require_longopt=True)
+    def test_all_message_ids(self, completion):
+        assert any("-" in x for x in completion)
+
+    @pytest.mark.complete("pylint --disable=", require_longopt=True)
+    def test_enabled_message_ids(self, completion):
+        assert any("-" in x for x in completion)
+
+    @pytest.mark.complete("pylint --enable=foo,", require_longopt=True)
+    def test_disabled_message_ids(self, completion):
+        assert any("-" in x for x in completion)
