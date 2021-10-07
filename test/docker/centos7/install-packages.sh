03eab0e6d29c8a53631f5ec0cc3c19faa57e75cd
Ville Skyttä <ville.skytta@iki.fi>
2021-10-07 23:35:39 +0300

    test: add CentOS 7 docker build files
    Based on the old docker-centos7 branch.

---
+++ test/docker/centos7/install-packages.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -xeuo pipefail
+
+shopt -s extglob
+
+cd ${TMPDIR:-/tmp}
+
+while read -r file; do
+    case $file in
+        mock | */mock) printf "%s\n" mock ;;
+        /*) printf "%s\n" "$file" ;;
+        *) printf "%s\n" {/usr,}/{,s}bin/"$file" ;;
+    esac
+done |
+    xargs yum -y install
