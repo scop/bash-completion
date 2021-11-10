37483148937016993657766094f3001eedf21d91
Ville Skyttä <ville.skytta@iki.fi>
2021-11-10 20:36:34 +0200

    feat(shellcheck): complete --severity/-S args

--- test/t/test_shellcheck.py
+++ test/t/test_shellcheck.py
@@ -17,3 +17,11 @@ class TestShellcheck:
     @pytest.mark.complete("shellcheck -s ", require_cmd=True)
     def test_4(self, completion):
         assert "bash" in completion
+
+    @pytest.mark.complete(
+        "shellcheck -S ",
+        require_cmd=True,
+        skipif="! shellcheck --help | command grep -qF -- --severity",
+    )
+    def test_severity_arg(self, completion):
+        assert completion
