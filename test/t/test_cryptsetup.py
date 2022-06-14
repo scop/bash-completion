2a527ca5a7bf823bfca518fe1a7857b81c9f2492
Koichi Murase <myoga.murase@gmail.com>
2022-06-14 17:57:10 +0900

    fix(cryptsetup): update the list of actions

--- test/t/test_cryptsetup.py
+++ test/t/test_cryptsetup.py
@@ -9,3 +9,7 @@ class TestCryptsetup:
     @pytest.mark.complete("cryptsetup -", require_cmd=True)
     def test_2(self, completion):
         assert completion
+
+    @pytest.mark.complete("cryptsetup luksE", require_cmd=True)
+    def test_github_issue758(self, completion):
+        assert completion == "rase"
