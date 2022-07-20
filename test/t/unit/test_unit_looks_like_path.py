f2b05116d76b353da990a38eef2be54ecc2a5519
Ville Skyttä <ville.skytta@iki.fi>
2022-07-20 23:45:30 +0300

    feat(_comp_looks_like_path): new utility, use it
    Be more consistent with how we test this across the tree.
    
    `apt-get`, `gdb`, `kldload`, `pydoc`, `pylint`, `removepkg`, `rpm`,
    `scp`, and `vpnc` gained better recognition of various kinds of paths as
    the result.

---
+++ test/t/unit/test_unit_looks_like_path.py
@@ -0,0 +1,31 @@
+import pytest
+
+from conftest import TestUnitBase, assert_bash_exec
+
+
+@pytest.mark.bashcomp(cmd=None)
+class TestUnitQuote(TestUnitBase):
+    @pytest.mark.parametrize(
+        "thing_looks_like",
+        (
+            ("", False),
+            ("foo", False),
+            ("/foo", True),
+            ("foo/", True),
+            ("foo/bar", True),
+            (".", True),
+            ("../", True),
+            ("~", True),
+            ("~foo", True),
+        ),
+    )
+    def test_1(self, bash, thing_looks_like):
+        thing, looks_like = thing_looks_like
+        output = assert_bash_exec(
+            bash,
+            f"_comp_looks_like_path '{thing}'; printf %s $?",
+            want_output=True,
+            want_newline=False,
+        )
+        is_zero = output.strip() == "0"
+        assert (looks_like and is_zero) or (not looks_like and not is_zero)
