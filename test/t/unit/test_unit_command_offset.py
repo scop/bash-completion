7b4413a5a5b2ba759ab741f875702afd7f4ca8e3
Koichi Murase <myoga.murase@gmail.com>
2022-08-26 19:57:38 +0900

    fix(_command_offset): modify $2 passed to completion functions
    Bash does not directly pass the current word, i.e.,
    ${COMP_WORDS[COMP_CWORD]}, to the seond argument of completion
    functions specified by `complete -F func` but instead modify the value
    of the current word with a certain rule.  External completion
    functions, which are not based on bash-completion, can possibly assume
    the modified value [1].
    
    The current implementation of `_command_offset` directly passes
    `${COMP_WORDS[COMP_CWORD]}` to the completion function extracted from
    `complete -p`, which may cause problems for not bash-completion-based
    external completions.
    
    [1] https://github.com/scop/bash-completion/issues/790
    
    Co-authored-by: Ville Skyttä <ville.skytta@iki.fi>

--- test/t/unit/test_unit_command_offset.py
+++ test/t/unit/test_unit_command_offset.py
@@ -2,7 +2,7 @@ from shlex import quote
 
 import pytest
 
-from conftest import assert_bash_exec, assert_complete
+from conftest import assert_bash_exec, assert_complete, bash_env_saved
 
 
 def join(words):
@@ -72,3 +72,171 @@ class TestUnitCommandOffset:
         cleared before the retry.
         """
         assert assert_complete(bash, "meta %s " % cmd) == expected_completion
+
+
+@pytest.mark.bashcomp(cmd=None, ignore_env=r"^[+-]ret=")
+class TestUnitCommandOffsetReduceCur:
+    def check(self, bash, cur, expected):
+        assert (
+            assert_bash_exec(
+                bash,
+                '_comp_command_offset__reduce_cur %s; echo "$ret"'
+                % quote(cur),
+                want_output=True,
+            ).strip("\r\n")
+            == expected
+        )
+
+    @pytest.mark.parametrize(
+        "cur,expected",
+        [
+            ("==", ""),
+            ("=:", ""),
+            ("--foo'=", "="),
+            ("=", ""),
+            ("'=", "="),
+            ("'='", "'='"),
+            ('a"b', "b"),
+            ('a"b"', 'a"b"'),
+            ('a"b"c', 'a"b"c'),
+            ('a"b"c"', ""),
+            ("a", "a"),
+            ("ab", "ab"),
+            ("abc", "abc"),
+            ("abcd", "abcd"),
+            ('a"a$(echo', "a$(echo"),
+            ('a"a$(echo "', 'a"a$(echo "'),
+            ('a"a$(echo "world"x', "x"),
+            ('a"a$(echo "world"x)', "x)"),
+            ("a${va", "a${va"),
+            ("$'a", "a"),
+            ("$'a\\n", "a\\n"),
+            ("$'a\\n\\'", "$'a\\n\\'"),
+            ("$'a\\n\\' '", ""),
+            ("$'a\\n\\' \\'x", "$'a\\n\\' \\'x"),
+            ("$'a\\n\\' \\'xyz'x", "x"),
+            ("a'bb\\'aaa", "a'bb\\'aaa"),
+            ("a'bb\\'aaa'c", "c"),
+            ('a"bb', "bb"),
+            ('a"bb\\"a', 'bb\\"a'),
+            ('a"bb\\"a"c', 'a"bb\\"a"c'),
+            ("a`", "a`"),
+            ("a`echo", "a`echo"),
+            ("a`echo w", "w"),
+            ('a"echo ', "echo "),
+            ('a"echo w', "echo w"),
+            ("$'a\\' x", "$'a\\' x"),
+            ("a`bbb ccc`", "ccc`"),
+            ("a`aa'a", "a"),
+            ('a`aa"aa', "aa"),
+            ("a`aa$'a\\'a a", "a`aa$'a\\'a a"),
+            ("a`b$'c\\'d e", "a`b$'c\\'d e"),
+            ("$'c\\'d e`f g", "$'c\\'d e`f g"),
+            ("$'c\\'d e'f`g h", "f`g h"),
+            ("$'a b'c`d e", "e"),
+            ("a`b'c'd e", "e"),
+            ("a`b'c'd e f", "f"),
+            ("a`$(echo world", "world"),
+            ("a`$'a\\' b", "a`$'a\\' b"),
+            ("a`$'b c\\'d e$'f g\\'", "g\\'"),
+            ("a`$'b c\\'d e$'f g\\'h i", "i"),
+            ("a`$'b c\\'d e$'f g\\'h i`j", "i`j"),
+            ("a`$'b c\\'d e'f g'", "g'"),
+            ("a`a;", ""),
+            ("a`x=", ""),
+            ("a`x=y", "y"),
+            ("a`b|", ""),
+            ("a`b:c", "c"),
+            ("a`b&", ""),
+        ],
+    )
+    def test_1(self, bash, cur, expected):
+        self.check(bash, cur, expected)
+
+    def test_COMP_WORDBREAKS_atmark(self, bash):
+        with bash_env_saved(bash) as bash_env:
+            bash_env.write_variable("COMP_WORDBREAKS", "@$IFS", quote=False)
+            self.check(bash, "a`b@", "@")
+            self.check(bash, "a`b@c", "@c")
+            self.check(bash, "a`b@@c", "@c")
+            self.check(bash, "a`b@@c@", "@")
+
+    def test_COMP_WORDBREAKS_z(self, bash):
+        with bash_env_saved(bash) as bash_env:
+            bash_env.write_variable(
+                "COMP_WORDBREAKS", "%s$IFS" % quote("z"), quote=False
+            )
+            self.check(bash, "a`b;c", "a`b;c")
+            self.check(bash, "a`bzc", "c")
+            self.check(bash, "a`bzcdze", "e")
+            self.check(bash, "a`bzcdzze", "e")
+            self.check(bash, "a`bzcdzzze", "e")
+            self.check(bash, "a`b\\zc", "a`b\\zc")
+
+    def test_COMP_WORDBREAKS_dollar(self, bash):
+        with bash_env_saved(bash) as bash_env:
+            bash_env.write_variable(
+                "COMP_WORDBREAKS", "%s$IFS" % quote("$"), quote=False
+            )
+            self.check(bash, "a`b$'hxy'", "a`b$'hxy'")
+            self.check(bash, "a`b$", "$")
+            self.check(bash, "a`b$x", "$x")
+            self.check(bash, "a`b${", "${")
+            self.check(bash, "a`b${x}", "${x}")
+            self.check(bash, "a`b${x}y", "${x}y")
+            self.check(bash, "a`b$=", "$=")
+            self.check(bash, "a`b$.", "$.")
+            self.check(bash, "a`b$'a'", "a`b$'a'")
+            self.check(bash, 'a`b$"a"', '$"a"')
+            self.check(bash, "a'b", "b")
+            self.check(bash, "a`b$'' a$'xyz", "xyz")
+
+    def test_COMP_WORDBREAKS_backslash(self, bash):
+        with bash_env_saved(bash) as bash_env:
+            bash_env.write_variable(
+                "COMP_WORDBREAKS", "%s$IFS" % quote("\\"), quote=False
+            )
+            self.check(bash, "a`b\\cd", "cd")
+            self.check(bash, "a`b\\cde\\fg", "fg")
+            self.check(bash, "a`b\\c\\\\a", "\\a")
+            self.check(bash, "a`b\\c\\\\\\a", "a")
+            self.check(bash, "a`b\\c\\\\\\\\a", "\\a")
+            self.check(bash, "a`b\\c\\a\\a", "a")
+            self.check(bash, "a`b\\", "")
+            self.check(bash, "a`b\\\\", "\\")
+            self.check(bash, "a`b\\\\\\", "")
+
+    def test_COMP_WORDBREAKS_dollar_backslash(self, bash):
+        with bash_env_saved(bash) as bash_env:
+            bash_env.write_variable(
+                "COMP_WORDBREAKS", "%s$IFS" % quote("$\\"), quote=False
+            )
+            self.check(bash, "a`b$\\", "")
+            self.check(bash, "a`b\\$", "$")
+
+    def test_COMP_WORDBREAKS_dollar_atmark_z(self, bash):
+        with bash_env_saved(bash) as bash_env:
+            bash_env.write_variable(
+                "COMP_WORDBREAKS", "%s$IFS" % quote("$@z"), quote=False
+            )
+            self.check(bash, "a$z", "")
+            self.check(bash, "a$$z", "")
+            self.check(bash, "a$$", "$")
+            self.check(bash, "a$@", "@")
+            self.check(bash, "a$$@", "@")
+
+    def test_COMP_WORDBREAKS_dollar_backquote(self, bash):
+        with bash_env_saved(bash) as bash_env:
+            bash_env.write_variable(
+                "COMP_WORDBREAKS", "%s$IFS" % quote("`"), quote=False
+            )
+            self.check(bash, "a`b`", "")
+
+    @pytest.mark.parametrize("symbol", list("!#%*+,-./?[]^_}~"))
+    def test_COMP_WORDBREAKS_symbol(self, bash, symbol):
+        with bash_env_saved(bash) as bash_env:
+            bash_env.write_variable(
+                "COMP_WORDBREAKS", "%s$IFS" % quote(symbol), quote=False
+            )
+            self.check(bash, "a`b%s" % symbol, "")
+            self.check(bash, "a`b%sc" % symbol, "c")
