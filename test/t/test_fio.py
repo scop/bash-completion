f91a5f6d6c4b228cb793a214021aca51afac61aa
Ville Skyttä <ville.skytta@iki.fi>
2022-09-09 17:15:37 +0300

    feat(fio): base support for option args per `--cmdhelp=COMMAND` output
    Supports options taking a boolean, as well as ones enumerating their
    valid values. For example:
    
    ```
    $` fio --cmdhelp=unlink
                  unlink: Unlink created files after job has completed
                    type: boolean value (opt=1)
                 default: 0
                   range: min=0, max=1
    $ fio --cmdhelp=kb_base
                 kb_base: Unit prefix interpretation for [...]
                    type: string (opt=bla)
                 default: 1024
            valid values: 1024       Inputs invert IEC and SI [...]
                        : 1000       Inputs use IEC and SI prefixes; [...]
    ```
    
    Ignore possibly suggested closest matches for unknown commands:
    
    ```
    $ fio --cmdhelp=x
    No such command: x - showing closest match
                      rw: IO direction
                      [...]
    ```

--- test/t/test_fio.py
+++ test/t/test_fio.py
@@ -23,3 +23,19 @@ class TestFio:
     def test_enghelp(self, completion):
         """Test --enghelp parsing."""
         assert completion
+
+    @pytest.mark.complete("fio --unlink=", require_cmd=True)
+    def test_cmdhelp_boolean(self, completion):
+        """Test --cmdhelp=COMMAND boolean parsing."""
+        assert completion == "0 1".split()
+
+    @pytest.mark.complete("fio --kb_base=", require_cmd=True)
+    def test_cmdhelp_valid_values(self, completion):
+        """Test --cmdhelp=COMMAND valid values parsing."""
+        # We expect kb_base args to be stable, no additions/removals.
+        assert completion == "1000 1024".split()
+
+    @pytest.mark.complete("fio --non_exist3nt_option=", require_cmd=True)
+    def test_cmdhelp_nonexistent(self, completion):
+        """Test --cmdhelp=COMMAND errors out gracefully."""
+        assert not completion
