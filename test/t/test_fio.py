b7a2fe079d6ec74c7215c63d6b0e9e3e1111a079
Ville Skyttä <ville.skytta@iki.fi>
2022-09-09 17:07:51 +0300

    feat(fio): offer options available from `--cmdhelp=all`
    A lot of options fio understands are not available from `--help`, but
    rather from `--cmdhelp=all` (as option names only).
    
    All of these seem to either take an argument, or do not mind an empty
    one, so offer all of them with the `=` suffix.

--- test/t/test_fio.py
+++ test/t/test_fio.py
@@ -13,3 +13,8 @@ class TestFio:
     @pytest.mark.complete("fio --debug=foo,")
     def test_3(self, completion):
         assert completion
+
+    @pytest.mark.complete("fio --ioengin", require_cmd=True)
+    def test_cmdhelp_all(self, completion):
+        """Test we got a "known present" option from --cmdhelp=all."""
+        assert completion == "e=" or "e" in completion
