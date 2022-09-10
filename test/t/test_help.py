01d2fadc94fd0a359955e33e9a9b8abc872f522a
Ville Skyttä <ville.skytta@iki.fi>
2022-09-10 22:19:38 +0300

    feat(help): support options, improve quoting
    The simple `-A helptopic` completion for does not support completion of
    options, and does not do a too good job with items containing parens,
    such as `(( ... ))` and `foo ((`.

---
+++ test/t/test_help.py
@@ -0,0 +1,18 @@
+import pytest
+
+
+class TestHelp:
+    @pytest.mark.complete("help ")
+    def test_basic(self, completion):
+        assert "help" in completion
+
+    @pytest.mark.complete("help -")
+    def test_options(self, completion):
+        assert completion
+
+    @pytest.mark.complete(r"help \(")
+    def test_parens(self, completion):
+        # Assumption: an item like "(( ... ))" exists in the output
+        assert any(
+            x.startswith(r"\(") and x.endswith(r"\)\)") for x in completion
+        )
