89c1b6844de443ad4cca23603cd503e6647dae1b
Ville Skyttä <ville.skytta@iki.fi>
2022-09-10 23:35:48 +0300

    feat(cd,pushd): add option completion support
    For both of these, `help -s` output is such that `_parse_usage` doesn't
    do a terribly good job with it. Parse the `help` (no `-s`) output with
    `_parse_help` instead.

--- test/t/test_pushd.py
+++ test/t/test_pushd.py
@@ -5,3 +5,7 @@ class TestPushd:
     @pytest.mark.complete("pushd ")
     def test_1(self, completion):
         assert completion
+
+    @pytest.mark.complete("pushd -")
+    def test_options(self, completion):
+        assert completion
