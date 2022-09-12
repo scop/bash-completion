eefef4474e9f13bae83f6d83f75b7903cfeca2d3
Ville Skyttä <ville.skytta@iki.fi>
2022-09-12 18:05:22 +0300

    feat(fio): more option and arg completions (#805)
    * feat(fio): offer options available from `--cmdhelp=all`
    
    A lot of options fio understands are not available from `--help`, but
    rather from `--cmdhelp=all` (as option names only).
    
    All of these seem to either take an argument, or do not mind an empty
    one, so offer all of them with the `=` suffix.
    
    * feat(fio): complete engines
    
    * feat(fio): complete --exec_post/prerun, --uid, and --gid args
    
    * feat(fio): base support for option args per `--cmdhelp=COMMAND` output
    
    Supports options taking a boolean, as well as ones enumerating their
    valid values. For example:
    
    ```
    $` fio --cmdhelp=unlink
                  unlink: Unlink created files after job has completed
                    type: boolean value (opt=1)
                 default: 0
                   range: min=0, max=1
    $ fio --cmdhelp=kb_base
                 kb_base: Unit prefix interpretation for [...]
                    type: string (opt=bla)
                 default: 1024
            valid values: 1024       Inputs invert IEC and SI [...]
                        : 1000       Inputs use IEC and SI prefixes; [...]
    ```
    
    Ignore possibly suggested closest matches for unknown commands:
    
    ```
    $ fio --cmdhelp=x
    No such command: x - showing closest match
                      rw: IO direction
                      [...]
    ```
    
    Co-authored-by: Koichi Murase <myoga.murase@gmail.com>

--- test/t/test_fio.py
+++ test/t/test_fio.py
@@ -13,3 +13,29 @@ class TestFio:
     @pytest.mark.complete("fio --debug=foo,")
     def test_3(self, completion):
         assert completion
+
+    @pytest.mark.complete("fio --ioengin", require_cmd=True)
+    def test_cmdhelp_all(self, completion):
+        """Test we got a "known present" option from --cmdhelp=all."""
+        assert completion == "e=" or "e" in completion
+
+    @pytest.mark.complete("fio --ioengine=", require_cmd=True)
+    def test_enghelp(self, completion):
+        """Test --enghelp parsing."""
+        assert completion
+
+    @pytest.mark.complete("fio --unlink=", require_cmd=True)
+    def test_cmdhelp_boolean(self, completion):
+        """Test --cmdhelp=COMMAND boolean parsing."""
+        assert completion == "0 1".split()
+
+    @pytest.mark.complete("fio --kb_base=", require_cmd=True)
+    def test_cmdhelp_valid_values(self, completion):
+        """Test --cmdhelp=COMMAND valid values parsing."""
+        # We expect kb_base args to be stable, no additions/removals.
+        assert completion == "1000 1024".split()
+
+    @pytest.mark.complete("fio --non_exist3nt_option=", require_cmd=True)
+    def test_cmdhelp_nonexistent(self, completion):
+        """Test --cmdhelp=COMMAND errors out gracefully."""
+        assert not completion
