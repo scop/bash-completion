dea48ff7bc9189d4352adf74f7b2783c323c636f
Ville Skyttä <ville.skytta@iki.fi>
2022-12-06 18:58:58 +0200

    feat(reportbug): parse various option args from option help
    Closes https://github.com/scop/bash-completion/pull/753

--- test/t/test_reportbug.py
+++ test/t/test_reportbug.py
@@ -5,3 +5,7 @@ class TestReportbug:
     @pytest.mark.complete("reportbug --m", require_cmd=True)
     def test_1(self, completion):
         assert completion
+
+    @pytest.mark.complete("reportbug --bts=", require_cmd=True)
+    def test_bts(self, completion):
+        assert "default" in completion
