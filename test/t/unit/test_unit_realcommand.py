a12755080ec1930bcc945eb384fcb6af20e850a8
Ville Skyttä <ville.skytta@iki.fi>
2023-04-10 15:21:55 +0300

    refactor: rename `_realcommand => _comp_realcommand`
    As a bonus, this implementation eliminates a superfluous `type -P` call.

--- test/t/unit/test_unit_realcommand.py
+++ test/t/unit/test_unit_realcommand.py
@@ -3,27 +3,45 @@ import pytest
 from conftest import assert_bash_exec, bash_env_saved
 
 
-@pytest.mark.bashcomp(cmd=None, cwd="shared", ignore_env=r"^\+COMPREPLY=")
+@pytest.mark.bashcomp(
+    cmd=None, cwd="shared", ignore_env=r"^\+declare -f __tester$"
+)
 class TestUnitRealCommand:
+    @pytest.fixture
+    def functions(self, bash):
+        assert_bash_exec(
+            bash,
+            (
+                "__tester() { "
+                "local ret rc; "
+                '_comp_realcommand "$1"; '
+                "rc=$?; "
+                'printf %s "$ret"; '
+                "return $rc; "
+                "}"
+            ),
+        )
+
     def test_non_pollution(self, bash):
         """Test environment non-pollution, detected at teardown."""
         assert_bash_exec(
             bash,
-            "foo() { local cur=; _realcommand bar; }; foo; unset -f foo",
+            "foo() { local ret=; _comp_realcommand bar; }; foo; unset -f foo",
             want_output=None,
         )
 
-    def test_basename(self, bash):
+    def test_basename(self, bash, functions):
         with bash_env_saved(bash) as bash_env:
             bash_env.write_variable("PATH", "$PWD/bin:$PATH", quote=False)
             output = assert_bash_exec(
                 bash,
-                "_realcommand arp",
+                "__tester arp",
                 want_output=True,
+                want_newline=False,
             )
             assert output.strip().endswith("/shared/bin/arp")
 
-    def test_basename_nonexistent(self, bash):
+    def test_basename_nonexistent(self, bash, functions):
         filename = "non-existent-file-for-bash-completion-tests"
         skipif = "! type -P %s" % filename
         try:
@@ -32,39 +50,41 @@ class TestUnitRealCommand:
             pytest.skipif(skipif)
         output = assert_bash_exec(
             bash,
-            "! _realcommand %s" % filename,
+            "! __tester %s" % filename,
             want_output=False,
         )
         assert output.strip() == ""
 
-    def test_relative(self, bash):
+    def test_relative(self, bash, functions):
         output = assert_bash_exec(
             bash,
-            "_realcommand bin/arp",
+            "__tester bin/arp",
             want_output=True,
+            want_newline=False,
         )
         assert output.strip().endswith("/shared/bin/arp")
 
-    def test_relative_nonexistent(self, bash):
+    def test_relative_nonexistent(self, bash, functions):
         output = assert_bash_exec(
             bash,
-            "! _realcommand bin/non-existent",
+            "! __tester bin/non-existent",
             want_output=False,
         )
         assert output.strip() == ""
 
-    def test_absolute(self, bash):
+    def test_absolute(self, bash, functions):
         output = assert_bash_exec(
             bash,
-            '_realcommand "$PWD/bin/arp"',
+            '__tester "$PWD/bin/arp"',
             want_output=True,
+            want_newline=False,
         )
         assert output.strip().endswith("/shared/bin/arp")
 
-    def test_absolute_nonexistent(self, bash):
+    def test_absolute_nonexistent(self, bash, functions):
         output = assert_bash_exec(
             bash,
-            '! _realcommand "$PWD/bin/non-existent"',
+            '! __tester "$PWD/bin/non-existent"',
             want_output=False,
         )
         assert output.strip() == ""
