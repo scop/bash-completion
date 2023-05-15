50df230265f3deb54a31a1f2aeb14d791939c5a9
Ville Skyttä <ville.skytta@iki.fi>
2023-05-15 22:17:15 +0300

    test(dcop): skip if basic invocation errors out
    It's highly unlikely that a DCOP server would be running in containers.

--- test/t/test_dcop.py
+++ test/t/test_dcop.py
@@ -2,6 +2,8 @@ import pytest
 
 
 class TestDcop:
-    @pytest.mark.complete("dcop ", require_cmd=True)
+    @pytest.mark.complete(
+        "dcop ", require_cmd=True, skipif="! dcop &>/dev/null"
+    )
     def test_1(self, completion):
         assert completion
