d651a8907b57c8acd7053e44d3a5ced263e013f1
Koichi Murase <myoga.murase@gmail.com>
2023-05-31 00:09:01 +0900

    refactor(_comp_ip_addresses): turn into a generator

--- bash_completion.d/000_bash_completion_compat.bash
+++ bash_completion.d/000_bash_completion_compat.bash
@@ -16,7 +16,7 @@ _comp_deprecate_func 2.12 _signals _comp_compgen_signals
 _comp_deprecate_func 2.12 _mac_addresses _comp_compgen_mac_addresses
 _comp_deprecate_func 2.12 _available_interfaces _comp_available_interfaces
 _comp_deprecate_func 2.12 _configured_interfaces _comp_compgen_configured_interfaces
-_comp_deprecate_func 2.12 _ip_addresses _comp_ip_addresses
+_comp_deprecate_func 2.12 _ip_addresses _comp_compgen_ip_addresses
 
 # Backwards compatibility for compat completions that use have().
 # @deprecated 1.90 should no longer be used; generally not needed with
