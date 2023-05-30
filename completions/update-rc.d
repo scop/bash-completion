848aa413273516529ccc163966f138ad3a6c53a0
Koichi Murase <myoga.murase@gmail.com>
2023-05-30 20:14:53 +0900

    fix(completions/*): quote array assignments properly

--- completions/update-rc.d
+++ completions/update-rc.d
@@ -31,7 +31,7 @@ _comp_cmd_update_rc_d()
         if [[ $cur == [0-9] || ! $cur ]]; then
             COMPREPLY=(0 1 2 3 4 5 6 7 8 9)
         elif [[ $cur == [0-9][0-9] ]]; then
-            COMPREPLY=($cur)
+            COMPREPLY=("$cur")
         else
             COMPREPLY=()
         fi
@@ -43,7 +43,7 @@ _comp_cmd_update_rc_d()
                 COMPREPLY=(0 1 2 3 4 5 6 S .)
             fi
         elif [[ $cur == [0-6S.] ]]; then
-            COMPREPLY=($cur)
+            COMPREPLY=("$cur")
         else
             COMPREPLY=()
         fi
