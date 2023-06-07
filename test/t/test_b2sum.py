cd985df6d8e613fd5421066248db1c50dc1686bd
Aaron Rogers <aaron.kyle.rogers@gmail.com>
2023-06-07 11:06:12 -0400

    feat(b2sum): new completion

---
+++ test/t/test_b2sum.py
@@ -0,0 +1,12 @@
+import pytest
+
+
+class TestB2sum:
+    @pytest.mark.complete("b2sum ")
+    def test_1(self, completion):
+        assert completion
+
+    @pytest.mark.complete("b2sum -", require_longopt=True)
+    def test_options(self, completion):
+        assert completion
+
