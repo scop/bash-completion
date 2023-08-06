57191e2731eb307434c2f11813d7dea303458dce
Ville Skyttä <ville.skytta@iki.fi>
2023-08-06 11:36:07 +0300

    fix(dict): nounset error when host/port not set

--- test/t/test_dict.py
+++ test/t/test_dict.py
@@ -5,3 +5,7 @@ class TestDict:
     @pytest.mark.complete("dict -", require_cmd=True)
     def test_1(self, completion):
         assert completion
+
+    @pytest.mark.complete("dict --database ", require_cmd=True)
+    def test_database(self, completion):
+        assert completion
