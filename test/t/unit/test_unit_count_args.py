29398efab4dd06fceb7c94c5329c5002b14cb8b4
Koichi Murase <myoga.murase@gmail.com>
2023-08-12 14:02:17 +0900

    perf(_comp_count_args): skip reinitializing cword and words
    In most of the contexts calling `_comp_count_args` in the current
    codebase, `cword` and `words` are already initialized with the same
    set of exclude chars (-n chars) by _comp_initialize, so there seems to
    be no need to again reassemble `cword` and `words`.  This patch
    removes the redundant initialization of `cword` and `words`.
    
    * When `_comp_initialize` is called without `-s` or `-n chars` and
      `_comp_count_args` is called with an empty $1, they are compatible
      so `cword` and `words` outside can be directly used.
    
    * `7z` and `nslookup` specify the same set of exclude chars as
      `_comp_initialize`, so existing `cword` and `words` are compatible.
    
    Nevertheless, we need to reinitialize `cword` and `words` when the
    specified exclude chars are different from those specified to
    _comp_initialize.
    
    * The `ssh` completion calls `_comp_count_args` with excluding `=`
      while it calls `_comp_initialize` with `-n :`.
    
    * The `chown` completion calls `_comp_count_args` with excluding `:`
      while it calls `_comp_initialize` with `-sn :`.
    
    * The `nc` completions calls `_comp_count_args` without exclude chars
      while it calls `_comp_initialize` with `-n :`.
    
    * The completions for `chmod`, `cryptsetup`, `hcitool`, `mkinitrd`,
      `patch`, `quota`, and `zopflipng` does not specify exclude chars to
      `_comp_count_args` while they specify `-s` to `_comp_initialize`.
    
    It is not clear whether these discrepancies are intended ones are not,
    but this patch tries to keep the current behavior by explicitly
    specifying the original exclude chars.

--- test/t/unit/test_unit_count_args.py
+++ test/t/unit/test_unit_count_args.py
@@ -4,50 +4,59 @@ from conftest import TestUnitBase, assert_bash_exec
 
 
 @pytest.mark.bashcomp(
-    cmd=None, ignore_env=r"^[+-](ret|COMP_(WORDS|CWORD|LINE|POINT))="
+    cmd=None,
+    ignore_env=r"^[+-](ret|cword|words|COMP_(WORDS|CWORD|LINE|POINT))=",
 )
 class TestUnitCountArgs(TestUnitBase):
-    def _test(self, *args, **kwargs):
-        return self._test_unit(
-            '_comp_count_args %s; echo "$ret"', *args, **kwargs
+    @pytest.fixture
+    def functions(self, bash):
+        assert_bash_exec(
+            bash,
+            '_comp__test_unit() { local -a words=(); local cword ret=""; _comp__reassemble_words "<>&" words cword; _comp_count_args "$@"; echo "$ret"; }',
         )
 
-    def test_1(self, bash):
-        assert_bash_exec(bash, "COMP_LINE= COMP_POINT=0 COMP_WORDS=() COMP_CWORD=; _comp_count_args")
+    def _test(self, *args, **kwargs):
+        return self._test_unit("_comp__test_unit %s", *args, **kwargs)
 
-    def test_2(self, bash):
+    def test_1(self, bash):
+        assert_bash_exec(
+            bash,
+            'COMP_LINE= COMP_POINT=0 COMP_WORDS=() COMP_CWORD=; _comp_count_args -n ""',
+        )
+
+    def test_2(self, bash, functions):
         """a b| should set args to 1"""
         output = self._test(bash, "(a b)", 1, "a b", 3)
         assert output == "1"
 
-    def test_3(self, bash):
+    def test_3(self, bash, functions):
         """a b|c should set args to 1"""
         output = self._test(bash, "(a bc)", 1, "a bc", 3)
         assert output == "1"
 
-    def test_4(self, bash):
+    def test_4(self, bash, functions):
         """a b c| should set args to 2"""
         output = self._test(bash, "(a b c)", 2, "a b c", 4)
         assert output == "2"
 
-    def test_5(self, bash):
+    def test_5(self, bash, functions):
         """a b| c should set args to 1"""
         output = self._test(bash, "(a b c)", 1, "a b c", 3)
         assert output == "1"
 
-    def test_6(self, bash):
+    def test_6(self, bash, functions):
         """a b -c| d should set args to 2"""
         output = self._test(bash, "(a b -c d)", 2, "a b -c d", 6)
         assert output == "2"
 
-    def test_7(self, bash):
+    def test_7(self, bash, functions):
         """a b -c d e| with -c arg excluded should set args to 2"""
         output = self._test(
             bash, "(a b -c d e)", 4, "a b -c d e", 10, arg='-a "@(-c|--foo)"'
         )
         assert output == "2"
 
-    def test_8(self, bash):
+    def test_8(self, bash, functions):
         """a -b -c d e| with -c arg excluded
         and -b included should set args to 1"""
         output = self._test(
@@ -60,7 +69,7 @@ class TestUnitCountArgs(TestUnitBase):
         )
         assert output == "2"
 
-    def test_9(self, bash):
+    def test_9(self, bash, functions):
         """a -b -c d e| with -b included should set args to 3"""
         output = self._test(
             bash, "(a -b -c d e)", 4, "a -b -c d e", 11, arg='-i "-b"'
