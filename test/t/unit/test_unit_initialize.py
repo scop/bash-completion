scop/bash-completion

commit 38150dbd9e2b80d6d4f8ce5b3e2e0f15f5ed5c79

fix: address shellcheck SC2295

bash_completion
@@ -466,7 +466,7 @@ _get_cword()
             if ((i < cword)); then
                 # No, cword lies further;
                 local old_size=${#cur}
-                cur=${cur#${words[i]}}
+                cur=${cur#"${words[i]}"}
                 local new_size=${#cur}
                 ((index -= old_size - new_size))
             fi
@@ -704,7 +704,7 @@ _comp_delimited()
         delimiter=$1
         shift
     fi
-    [[ $cur == *$delimiter* ]] && prefix=${cur%$delimiter*}$delimiter
+    [[ $cur == *$delimiter* ]] && prefix=${cur%"$delimiter"*}$delimiter
 
     if $deduplicate; then
         # We could construct a -X pattern to feed to compgen, but that'd
@@ -727,10 +727,10 @@ _comp_delimited()
                     fi
                 done
             done
-            COMPREPLY=($(compgen -W '"${COMPREPLY[@]}"' -- "${cur##*$delimiter}"))
+            COMPREPLY=($(compgen -W '"${COMPREPLY[@]}"' -- "${cur##*"$delimiter"}"))
         fi
     else
-        COMPREPLY=($(compgen "$@" -- "${cur##*$delimiter}"))
+        COMPREPLY=($(compgen "$@" -- "${cur##*"$delimiter"}"))
     fi
 
     ((${#COMPREPLY[@]} == 1)) && COMPREPLY=(${COMPREPLY/#/$prefix})
@@ -853,7 +853,7 @@ _init_completion()
                 esac
                 ;;
         esac
-        cur=${cur##$redir}
+        cur=${cur##"$redir"}
         _filedir $xspec
         return 1
     fi
@@ -1011,7 +1011,7 @@ _parse_usage()
 # @param $1 prefix
 _signals()
 {
-    local -a sigs=($(compgen -P "${1-}" -A signal "SIG${cur#${1-}}"))
+    local -a sigs=($(compgen -P "${1-}" -A signal "SIG${cur#"${1-}"}"))
     COMPREPLY+=("${sigs[@]/#${1-}SIG/${1-}}")
 }
 
@@ -1975,10 +1975,10 @@ _cd()
         # create an array of matched subdirs
         k=${#COMPREPLY[@]}
         for j in $(compgen -d -- $i/$cur); do
-            if [[ ($mark_symdirs && -L $j || $mark_dirs && ! -L $j) && ! -d ${j#$i/} ]]; then
+            if [[ ($mark_symdirs && -L $j || $mark_dirs && ! -L $j) && ! -d ${j#"$i/"} ]]; then
                 j+="/"
             fi
-            COMPREPLY[k++]=${j#$i/}
+            COMPREPLY[k++]=${j#"$i/"}
         done
     done
 
@@ -2094,7 +2094,7 @@ _command_offset()
                     cspec=${cspec#*-o }
                     opt=${cspec%% *}
                     compopt -o $opt
-                    cspec=${cspec#$opt}
+                    cspec=${cspec#"$opt"}
                 done
             else
                 cspec=${cspec#complete}

completions/_mount
@@ -47,7 +47,7 @@ _mount()
                 "$(smbclient -d 0 -NL $host 2>/dev/null |
                     command sed -ne '/^[[:blank:]]*Sharename/,/^$/p' |
                     command sed -ne '3,$s|^[^A-Za-z]*\([^[:blank:]]*\).*$|/\1|p')" \
-                -- "${cur#//$host}"))
+                -- "${cur#//"$host"}"))
         fi
     elif [[ -r /etc/vfstab ]]; then
         # Solaris

completions/_mount.linux
@@ -205,7 +205,7 @@ _mount()
                     ;;&
             esac
             # COMP_WORDBREAKS is a real pain in the ass
-            prev="${prev##*[$COMP_WORDBREAKS]}"
+            prev="${prev##*["$COMP_WORDBREAKS"]}"
             $split && COMPREPLY=(${COMPREPLY[@]/#/"$prev,"})
             [[ ${COMPREPLY-} == *= ]] && compopt -o nospace
             return
@@ -241,7 +241,7 @@ _mount()
                 "$(smbclient -d 0 -NL $host 2>/dev/null |
                     command sed -ne '/^[[:blank:]]*Sharename/,/^$/p' |
                     command sed -ne '3,$s|^[^A-Za-z]*\([^[:blank:]]*\).*$|/\1|p')" \
-                -- "${cur#//$host}"))
+                -- "${cur#//"$host"}"))
         fi
     fi
 

completions/mplayer
@@ -89,7 +89,7 @@ _mplayer()
             for i in ~/.mplayer/skins "${dirs[@]}"; do
                 if [[ -d $i && -r $i ]]; then
                     for j in $(compgen -d -- $i/$cur); do
-                        COMPREPLY[k++]=${j#$i/}
+                        COMPREPLY[k++]=${j#"$i/"}
                     done
                 fi
             done

completions/perl
@@ -40,7 +40,7 @@ _perl()
                 ;;
             -*[mM])
                 temp="${cur#-}"
-                prefix+="${cur%$temp}"
+                prefix+=${cur%"$temp"}
                 cur="$temp"
                 _perl_helper modules $1
                 return
@@ -48,7 +48,7 @@ _perl()
             -*V)
                 if [[ $cur == :* ]]; then
                     temp="${cur##+(:)}"
-                    prefix+="${cur%$temp}"
+                    prefix+=${cur%"$temp"}
                     local IFS=$'\n'
                     COMPREPLY=($(compgen -P "$prefix" -W \
                         '$($1 -MConfig -e "print join \"\\n\",
@@ -60,7 +60,7 @@ _perl()
             -*d | -*dt)
                 if [[ $cur == :* ]]; then
                     temp="${cur#:}"
-                    prefix="$prefix${cur%$temp}"
+                    prefix=$prefix${cur%"$temp"}
                     cur="Devel::$temp"
                     _perl_helper modules $1
                 fi

completions/ri
@@ -80,8 +80,8 @@ _ri()
     if [[ $cur == [A-Z]*[#.]* ]]; then
         [[ $cur == *#* ]] && separator=# || separator=.
         # we're completing on class and method
-        class=${cur%$separator*}
-        method=${cur#*$separator}
+        class=${cur%"$separator"*}
+        method=${cur#*"$separator"}
         classes=($class)
         prefix="-P $class$separator"
         _ri_get_methods

completions/svcadm
@@ -39,7 +39,7 @@ _smf_complete_fmri()
     for fmri in $(svcs -H -o FMRI "$pattern" 2>/dev/null); do
         local fmri_part_list fmri_part
         if [[ -z $exact_mode ]]; then
-            fmri=${fmri#$prefix/}
+            fmri=${fmri#"$prefix/"}
 
             # we generate all possibles abbreviations for the FMRI
             # no need to have a generic loop as we will have a finite
@@ -62,7 +62,7 @@ _smf_complete_fmri()
         # we cut them at the first slash
         for fmri_part in $fmri_part_list; do
             [[ $fmri_part == $cur* ]] || continue
-            local first_part=${fmri_part#$cur_prefix}
+            local first_part=${fmri_part#"$cur_prefix"}
             first_part=$cur_prefix${first_part%%/*}
             [[ $first_part != "$fmri_part" ]] && first_part+="/"
             fmri_list+=" $first_part"

completions/tar
@@ -168,7 +168,7 @@ __tar_parse_old_opt()
             if __tar_is_argreq "$char"; then
                 old_opt_parsed+=("$char")
             fi
-            first_word=${first_word##$char}
+            first_word=${first_word##"$char"}
             char=${first_word:0:1}
         done
     fi

commit da16bf61c2604ea4efa9b2d125aedd2dd4acd4e5

fix(_comp_initialize): fix completions of redirections without space
This is a regression by commit 38150dbd.  Although the shellcheck
issues a warning SC2295, $redir is intensionally a pattern here.

Fixes https://github.com/scop/bash-completion/issues/832

bash_completion
@@ -1382,7 +1382,8 @@ _comp_initialize()
                 esac
                 ;;
         esac
-        cur=${cur##"$redir"}
+        # shellcheck disable=SC2295 # redir is a pattern
+        cur=${cur##$redir}
         _comp_compgen_filedir "$xspec"
         return 1
     fi

test/t/unit/test_unit_initialize.py
@@ -57,3 +57,10 @@ class TestUnitInitCompletion(TestUnitBase):
             bash, "cmd1 %s f" % redirect, cwd="shared/default"
         )
         assert "foo" in completion
+
+    @pytest.mark.parametrize("redirect", "> >> 2> < &>".split())
+    def test_redirect_3(self, bash, redirect):
+        completion = assert_complete(
+            bash, "cmd1 %sf" % redirect, cwd="shared/default"
+        )
+        assert "foo" in completion