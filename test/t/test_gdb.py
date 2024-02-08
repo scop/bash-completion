e18d1f8d124a44ee45f5dafef7b521e75eb1abf5
Koichi Murase <myoga.murase@gmail.com>
2024-02-08 16:05:54 +0900

    fix(gdb): fix regression that fails to generate command names
    The current version fails to generate any command names for the first
    word after the command name "gdb".  This is a regression introduced in
    
    https://github.com/scop/bash-completion/pull/1086
    
    and pointed out in the comment:
    
    https://github.com/scop/bash-completion/commit/73c5292b7a39a2f3d1ae01057e0afd3aa24c8a32#r138406647
    
    Co-authored-by: Grisha Levit <grishalevit@gmail.com>

--- test/t/test_gdb.py
+++ test/t/test_gdb.py
@@ -12,3 +12,13 @@ class TestGdb:
             "core core.12345 "
             "core.weston.1000.deadbeef.5308.1555362132000000".split()
         )
+
+    @pytest.mark.complete("gdb aw")
+    def test_3(self, completion):
+        """Check that the completion can generate command names"""
+        assert completion == ["k"] or "awk" in completion
+
+    @pytest.mark.complete("gdb built")
+    def test_4(self, completion):
+        """Check that the completion does not generate builtin names"""
+        assert not (completion == ["in"] or "builtin" in completion)
