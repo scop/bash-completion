7d25eb1ad6a32e9dcb961125534b378d55125b20
Yedaya Katsman <yedaya.ka@gmail.com>
2024-02-12 20:59:07 +0200

    feat(ip): Add completion for monitor subcommand
    Currently it stops after completing the first option, although you
    should be able to choose multiple options.
    
    Also include a test that is skipped if the monitor subcommand doesn't
    exist.

--- test/t/test_ip.py
+++ test/t/test_ip.py
@@ -14,6 +14,15 @@ class TestIp:
     def test_r_r(self, completion):
         assert completion
 
+    @pytest.mark.complete(
+        "ip monitor ",
+        require_cmd=True,
+        skipif="ip monitor help 2>/dev/null; (( $? != 255 ))",
+    )
+    def test_monitor(self, completion):
+        assert "neigh" in completion
+        assert "all" in completion
+
     @pytest.mark.complete("ip -", require_cmd=True)
     def test_options(self, completion):
         assert "-family" in completion
