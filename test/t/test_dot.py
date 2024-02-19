be0010e8517de89bf06bda02bdccacc51968136e
Koichi Murase <myoga.murase@gmail.com>
2024-02-19 12:43:17 +0900

    feat(dot): support filename extension .gv
    The dot command from graphviz used .dot for the filename extension in
    the past, but .gv is the preferred extension.
    
    References:
    
    https://github.com/scop/bash-completion/issues/1109

--- test/t/test_dot.py
+++ test/t/test_dot.py
@@ -5,3 +5,11 @@ class TestDot:
     @pytest.mark.complete("dot ")
     def test_1(self, completion):
         assert completion
+
+    @pytest.mark.complete("dot t", cwd="dot")
+    def test_2(self, completion):
+        assert completion == ["test1.gv", "test2.dot"]
+
+    @pytest.mark.complete("dot test1", cwd="dot")
+    def test_3(self, completion):
+        assert completion == ".gv"
