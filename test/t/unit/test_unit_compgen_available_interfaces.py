6de75e224ae29e732a57bc47d8592c874848ebee
Koichi Murase <myoga.murase@gmail.com>
2024-03-10 11:40:57 +0900

    fix(available_interfaces): fix regression of unwanted trailing colons
    This is a fix for regression introduced in commit b6035350 and
    reported in GitHub #1129 [1].  The trailing colons of the generated
    interface names were removed before, but not they are not removed.
    
    [1] https://github.com/scop/bash-completion/issues/1129
    
    In addition, generated words can have the form `veth0@veth1`, where we
    want only the first part `veth0` not containing any punctuation
    characters.
    
    In this patch, we remove any [[:punct:]] and all the later characters
    in the generated words.

---
+++ test/t/unit/test_unit_compgen_available_interfaces.py
@@ -0,0 +1,25 @@
+import pytest
+
+from conftest import assert_bash_exec
+
+
+@pytest.mark.bashcomp(cmd=None)
+class TestUtilCompgenAvailableInterfaces:
+    @pytest.fixture
+    def functions(self, bash):
+        assert_bash_exec(
+            bash,
+            "_comp__test_dump() { ((${#arr[@]})) && printf '<%s>' \"${arr[@]}\"; echo; }",
+        )
+        assert_bash_exec(
+            bash,
+            '_comp__test_compgen() { local -a arr=(00); _comp_compgen -v arr "$@"; _comp__test_dump; }',
+        )
+
+    def test_1_trailing_colons(self, bash, functions):
+        output = assert_bash_exec(
+            bash,
+            "_comp__test_compgen available_interfaces",
+            want_output=True,
+        )
+        assert ":" not in output.strip()
