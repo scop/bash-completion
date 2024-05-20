1418b1991012a8d20e8850c62a57c432bd9bcdf3
Jiří Pejchal <jiri.pejchal@gmail.com>
2024-05-20 15:34:16 +0200

    feat(java): enable .java filename completion for java command
    Modify java command completion scripts to also complete filenames
    with the .java extension. This adds support for
    - JEP 330: Launch Single-File Source-Code Programs
    - JEP 458: Launch Multi-File Source-Code Programs
    
    Closes #1196
    
    Co-authored-by: frankslin <frankslin@users.noreply.github.com>

---
+++ test/fixtures/java/JEP330.java
@@ -0,0 +1,12 @@
+/**
+ * The java launcher is able to run a program
+ * supplied as a single file of Java source code.
+ *
+ * @see <a href="https://openjdk.java.net/jeps/330">JEP 330: Launch Single-File Source-Code Programs</a>
+ * @see <a href="https://openjdk.java.net/jeps/458">JEP 458: Launch Multi-File Source-Code Programs</a>
+ */
+class JEP330 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
