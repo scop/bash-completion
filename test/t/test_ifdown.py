762f410ac2712cf2a1402d6e4bb046a580111fba
Yedaya Katsman <yedaya.ka@gmail.com>
2024-06-03 22:14:59 +0300

    test(ifdown,iftest): Skip if we don't have network
    Without network we likely don't have any interfaces configured either
    
    Fixes: https://github.com/scop/bash-completion/issues/479

--- test/t/test_ifdown.py
+++ test/t/test_ifdown.py
@@ -1,9 +1,15 @@
+import os
+
 import pytest
 
 from conftest import in_container
 
 
 class TestIfdown:
+    @pytest.mark.xfail(
+        os.environ.get("NETWORK") == "none",
+        reason="There won't be any configured interfaces without network",
+    )
     @pytest.mark.xfail(in_container(), reason="Probably fails in a container")
     @pytest.mark.complete("ifdown ")
     def test_1(self, completion):
