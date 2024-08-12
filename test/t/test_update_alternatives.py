fc79351d55112c6b8b266e09fcd2f4454a9b55f8
Koichi Murase <myoga.murase@gmail.com>
2024-08-12 11:18:34 +0900

    fix(update-alternatives): fix the "--help" parsing
    The format of `update-alternatives --help` and `alternative --help` is
    not in the form expected by `_comp_compgen_help`.  This adjusts the
    output of `"$1" --help` before passing the output to
    `_comp_compgen_help`.

--- test/t/test_update_alternatives.py
+++ test/t/test_update_alternatives.py
@@ -6,3 +6,7 @@ class TestUpdateAlternatives:
     @pytest.mark.complete("update-alternatives --", require_cmd=True)
     def test_1(self, completion):
         assert completion
+
+    @pytest.mark.complete("update-alternatives --", require_cmd=True)
+    def test_2(self, bash, completion):
+        assert all(x in completion for x in ["--list", "--remove"])
