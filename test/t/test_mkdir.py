0071cca341bb988ff073dd5b77f6e06c3c818850
Yedaya Katsman <yedaya.ka@gmail.com>
2024-11-28 23:21:37 +0200

    test(mkdir): Remove xfail, align tests with others

--- test/t/test_mkdir.py
+++ test/t/test_mkdir.py
@@ -10,11 +10,9 @@ class TestMkdir:
     def test_2(self, completion):
         assert completion == ["bar", "bar bar.d/", "foo", "foo.d/"]
 
-    @pytest.mark.xfail  # TODO: why path in completion, basename in .output?
     @pytest.mark.complete("mkdir shared/default/foo.d/")
     def test_3(self, completion):
-        assert completion.output == "foo"
-        assert completion == [completion.output]
+        assert completion == ["foo"]
 
     @pytest.mark.complete("mkdir -", require_longopt=True)
     def test_options(self, completion):
