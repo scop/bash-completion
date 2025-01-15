d3ff6a7081c53778529f5bad3e0846f12b29bc74
Yedaya Katsman <yedaya.ka@gmail.com>
2025-01-15 23:29:54 +0200

    feat(wget): ipv6 addresses for --bind-address
    Fixes https://github.com/scop/bash-completion/issues/1315

--- test/t/test_wget.py
+++ test/t/test_wget.py
@@ -9,3 +9,8 @@ class TestWget:
     @pytest.mark.complete("wget --s", require_cmd=True)
     def test_2(self, completion):
         assert completion
+
+    @pytest.mark.complete("wget --bind-address=:")
+    def test_3(self, completion):
+        # Binding to ipv6 localhost
+        assert ":1" in completion
