5f08f4a780e57d0e8dd840ebc6c96c3112da34cf
Yedaya Katsman <yedaya.ka@gmail.com>
2025-04-09 22:26:12 +0300

    ci(fedora): Fix syntax for dnf5
    The path for the transaction history sqlite moved, the options have to
    be after the install subcommand

--- test/docker/fedoradev/install-packages.sh
+++ test/docker/fedoradev/install-packages.sh
@@ -19,7 +19,7 @@ while read -r file; do
         *) printf "%s\n" {/usr,}/{,s}bin/"$file" ;;
     esac
 done |
-    xargs dnf --skip-broken -y install
+    xargs dnf -y install --skip-unavailable --skip-broken
 # --skip-broken: avoid failing on not found packages. Also prevents actually
 # broken packages from failing the install which is not what we want, but
 # there doesn't seem to be way to cleanly just skip the not found ones.
