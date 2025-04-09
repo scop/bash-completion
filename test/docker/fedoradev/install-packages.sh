0adcc1aa48814ea0c47251dd0bab30c06b702e02
Yedaya Katsman <yedaya.ka@gmail.com>
2025-04-09 22:26:12 +0300

    ci(fedora): Fix syntax for dnf5, don't use sbin
    sbin is being unified with bin fedora:
    https://fedoraproject.org/wiki/Changes/Unify_bin_and_sbin
    The path for the transaction history sqlite moved as well

--- test/docker/fedoradev/install-packages.sh
+++ test/docker/fedoradev/install-packages.sh
@@ -16,10 +16,10 @@ dnf -y install /usr/bin/xargs
 while read -r file; do
     case $file in
         /*) printf "%s\n" "$file" ;;
-        *) printf "%s\n" {/usr,}/{,s}bin/"$file" ;;
+        *) printf "%s\n" {/usr,}/bin/"$file" ;;
     esac
 done |
-    xargs dnf --skip-broken -y install
+    xargs dnf -y install --skip-unavailable --skip-broken
 # --skip-broken: avoid failing on not found packages. Also prevents actually
 # broken packages from failing the install which is not what we want, but
 # there doesn't seem to be way to cleanly just skip the not found ones.
