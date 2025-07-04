0eabf4fc7bb03caa9557465eab2c45eb1a9481b8
Yedaya Katsman <yedaya.ka@gmail.com>
2025-07-04 14:33:47 +0300

    test(make): Move file deletion to a fixture

--- test/t/test_make.py
+++ test/t/test_make.py
@@ -6,20 +6,23 @@ from conftest import assert_complete
 
 
 class TestMake:
+    @pytest.fixture
+    def remove_extra_makefile(self, bash):
+        yield
+        os.remove(f"{bash.cwd}/make/extra_makefile")
+
     @pytest.mark.complete("make -f Ma", cwd="make")
     def test_1(self, completion):
         assert completion == "kefile"
 
     @pytest.mark.complete("make .", cwd="make", require_cmd=True)
-    def test_2(self, bash, completion):
+    def test_2(self, bash, completion, remove_extra_makefile):
         """Hidden targets."""
         assert completion == ".cache/ .test_passes".split()
-        os.remove(f"{bash.cwd}/make/extra_makefile")
 
     @pytest.mark.complete("make .cache/", cwd="make", require_cmd=True)
-    def test_3(self, bash, completion):
+    def test_3(self, bash, completion, remove_extra_makefile):
         assert completion == ".cache/1 .cache/2".split()
-        os.remove(f"{bash.cwd}/make/extra_makefile")
 
     @pytest.mark.complete("make ", cwd="shared/empty_dir")
     def test_4(self, completion):
@@ -30,24 +33,20 @@ class TestMake:
         assert completion
 
     @pytest.mark.complete("make ", cwd="make", require_cmd=True)
-    def test_6(self, bash, completion):
+    def test_6(self, bash, completion, remove_extra_makefile):
         assert completion == "all clean extra_makefile install sample".split()
-        os.remove(f"{bash.cwd}/make/extra_makefile")
 
     @pytest.mark.complete("make .cache/.", cwd="make", require_cmd=True)
-    def test_7(self, bash, completion):
+    def test_7(self, bash, completion, remove_extra_makefile):
         assert completion == ".cache/.1 .cache/.2".split()
-        os.remove(f"{bash.cwd}/make/extra_makefile")
 
     @pytest.mark.complete("make -C make ", require_cmd=True)
-    def test_8(self, bash, completion):
+    def test_8(self, bash, completion, remove_extra_makefile):
         assert completion == "all clean extra_makefile install sample".split()
-        os.remove(f"{bash.cwd}/make/extra_makefile")
 
     @pytest.mark.complete("make -nC make ", require_cmd=True)
-    def test_8n(self, bash, completion):
+    def test_8n(self, bash, completion, remove_extra_makefile):
         assert completion == "all clean extra_makefile install sample".split()
-        os.remove(f"{bash.cwd}/make/extra_makefile")
 
     @pytest.mark.complete("make -", require_cmd=True)
     def test_9(self, completion):
