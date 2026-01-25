c1e3f04cb1166e9ae62e1439438766d030cbf5eb
Koichi Murase <myoga.murase@gmail.com>
2026-01-25 18:54:10 +0900

    test(service): use dir "test/fixtures/_comp_compgen_xinetd_services"
    Instead of relying on service commands or directories existing
    
    Co-authored-by: Ville Skyttä <ville.skytta@iki.fi>

--- test/t/test_service.py
+++ test/t/test_service.py
@@ -2,12 +2,19 @@ import sys
 
 import pytest
 
+from conftest import assert_complete, bash_env_saved
+
 
 class TestService:
     @pytest.mark.xfail(
         sys.platform == "darwin",
         reason="Service completion not available on macOS",
     )
-    @pytest.mark.complete("service ")
-    def test_1(self, completion):
-        assert completion
+    def test_1(self, bash):
+        with bash_env_saved(bash) as bash_env:
+            bash_env.write_variable(
+                "_comp__test_inetd_dir",
+                "%s/_comp_compgen_xinetd_services/xinetd.d" % bash.cwd,
+            )
+            completion = assert_complete(bash, "service ")
+            assert all(x in completion for x in ("arp", "ifconfig"))
