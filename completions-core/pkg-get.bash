b428c92def44351849a70495f62ae83b806b3e83
Koichi Murase <myoga.murase@gmail.com>
2026-06-28 20:08:17 +0900

    refactor(arch,gcc,pkg-get,screen): reduce extra indentation (#1651)
    The completion "arch.bash" had extra indentation for conditional
    definition of the completion setting.  This can be replaced by an
    early return, and the indentation of the entire file can be reduced.
    A similar treatment has already been done in `chflags`, `ifup`,
    `killall` `route`, `watch`, `modules`, etc.
    
    The completions "{gcc,pkg-get,screen}.bash" have indented functions
    chained by the && operator.  A function definition may fail when there
    is an existing readonly shell function of the same name, but other
    completions have never cared about the function-definition failure for
    helper functions.  Even "{gcc,pkg-get,screen}.bash" do not seem to
    care about the error messages caused by the possible readonly shell
    functions.

--- completions-core/pkg-get.bash
+++ completions-core/pkg-get.bash
@@ -22,49 +22,50 @@ _comp_cmd_pkg_get__catalog_file()
     REPLY="${url##*//}"
     REPLY="${REPLY%%/*}"
     REPLY="/var/pkg-get/catalog-$REPLY"
+}
+
+_comp_cmd_pkg_get()
+{
+    local cur prev file url command=""
+    COMPREPLY=()
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    prev="${COMP_WORDS[COMP_CWORD - 1]}"
+
+    if [[ ${prev} == "-s" ]]; then
+        return 1
+    fi
+
+    local i=${#COMP_WORDS[*]}
+    while ((i > 0)); do
+        if [[ ${COMP_WORDS[--i]} == -s ]]; then
+            url="${COMP_WORDS[i + 1]}"
+        fi
+        if [[ ${COMP_WORDS[i]} == @(-[aDdiUu]|available|describe|download|install|list|updatecatalog|upgrade) ]]; then
+            command="${COMP_WORDS[i]}"
+        fi
+    done
+
+    if [[ $command ]]; then
+        if [[ $command == @(-[Ddi]|describe|download|install) ]]; then
+            local REPLY
+            _comp_cmd_pkg_get__catalog_file "$url"
+            if [[ -f $REPLY ]]; then
+                local packages_list=$(_comp_awk '$0 ~ /BEGIN PGP SIGNATURE/ { exit } $1 ~ /^Hash:/ || $1 ~ /^ *(-|#|$)/ { next } { print $1 }' "$REPLY")
+                _comp_compgen -- -W "${packages_list}"
+            fi
+        fi
+        return
+    fi
+
+    if [[ ${cur} == -* ]]; then
+        local -a opts=(-c -d -D -f -i -l -s -S -u -U -v)
+        _comp_compgen -- -W '"${opts[@]}"'
+        return
+    fi
+
+    local -a commands=(
+        available describe download install list updatecatalog upgrade
+    )
+    _comp_compgen -- -W '"${commands[@]}"'
 } &&
-    _comp_cmd_pkg_get()
-    {
-        local cur prev file url command=""
-        COMPREPLY=()
-        cur="${COMP_WORDS[COMP_CWORD]}"
-        prev="${COMP_WORDS[COMP_CWORD - 1]}"
-
-        if [[ ${prev} == "-s" ]]; then
-            return 1
-        fi
-
-        local i=${#COMP_WORDS[*]}
-        while ((i > 0)); do
-            if [[ ${COMP_WORDS[--i]} == -s ]]; then
-                url="${COMP_WORDS[i + 1]}"
-            fi
-            if [[ ${COMP_WORDS[i]} == @(-[aDdiUu]|available|describe|download|install|list|updatecatalog|upgrade) ]]; then
-                command="${COMP_WORDS[i]}"
-            fi
-        done
-
-        if [[ $command ]]; then
-            if [[ $command == @(-[Ddi]|describe|download|install) ]]; then
-                local REPLY
-                _comp_cmd_pkg_get__catalog_file "$url"
-                if [[ -f $REPLY ]]; then
-                    local packages_list=$(_comp_awk '$0 ~ /BEGIN PGP SIGNATURE/ { exit } $1 ~ /^Hash:/ || $1 ~ /^ *(-|#|$)/ { next } { print $1 }' "$REPLY")
-                    _comp_compgen -- -W "${packages_list}"
-                fi
-            fi
-            return
-        fi
-
-        if [[ ${cur} == -* ]]; then
-            local -a opts=(-c -d -D -f -i -l -s -S -u -U -v)
-            _comp_compgen -- -W '"${opts[@]}"'
-            return
-        fi
-
-        local -a commands=(
-            available describe download install list updatecatalog upgrade
-        )
-        _comp_compgen -- -W '"${commands[@]}"'
-    } &&
-        complete -F _comp_cmd_pkg_get pkg-get
+    complete -F _comp_cmd_pkg_get pkg-get
